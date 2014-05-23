f127e9bcd7d6df3a1b0b978846e4fbea7af4ea3b
Josh Blum <josh@joshknows.com>
2014-05-22 18:54:39 -0700

    pothos: helpful subtree pusher upload script

---
+++ ultimate_subtree_push.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+BRANCH=$1
+
+COMPONENTS="
+pothos-opencl
+pothos-managed
+pothos-widgets
+pothos-gui
+pothos-library
+pothos-python
+pothos-util
+pothos-java
+pothos-blocks
+pothos-serialization
+"
+
+for PREFIX in $COMPONENTS; do
+    git subtree push --prefix=${PREFIX} git@github.com:pothosware/${PREFIX}.git ${BRANCH}
+done
