e28968b8c8a83eb43f0edfa447992fc6360d9aa3
Josh Blum <josh@joshknows.com>
2014-06-18 01:30:16 -0400

    pothos-blocks: added copier block for domain crossing

---
+++ pothos-blocks/misc/Copier.cpp
@@ -0,0 +1,55 @@
+// Copyright (c) 2014-2014 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include <Pothos/Framework.hpp>
+#include <cstring> //memset
+
+/***********************************************************************
+ * |PothosDoc Copier
+ *
+ * The forwarder block copies all data from input port 0 to the output port 0.
+ * This block is used to bridge connections between incompatible domains.
+ *
+ * |category /Misc
+ * |keywords copier
+ *
+ * |factory /blocks/misc/copier()
+ **********************************************************************/
+class Copier : public Pothos::Block
+{
+public:
+    static Block *make(void)
+    {
+        return new Copier();
+    }
+
+    Copier(void)
+    {
+        this->setupInput(0);
+        this->setupOutput(0);
+    }
+
+    void work(void)
+    {
+        auto inputPort = this->input(0);
+        auto outputPort = this->output(0);
+
+        if (inputPort->hasMessage())
+        {
+            auto m = inputPort->popMessage();
+            outputPort->postMessage(m);
+        }
+
+        auto elems = this->workInfo().minElements;
+        if (elems == 0) return;
+        std::memcpy(
+            outputPort->buffer().as<void *>(),
+            inputPort->buffer().as<const void *>(),
+            elems*1);
+        inputPort->consume(elems);
+        outputPort->produce(elems);
+    }
+};
+
+static Pothos::BlockRegistry registerCopier(
+    "/blocks/misc/copier", &Copier::make);
