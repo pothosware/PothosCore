7fd95029613b0ca55dd7886f7fa46010fedd4bc7
Josh Blum <josh@joshknows.com>
2014-06-29 18:07:52 -0400

    pothos-blocks: adder block to test feedback loops

---
+++ pothos-blocks/arithmetic/Add.cpp
@@ -0,0 +1,118 @@
+// Copyright (c) 2014-2014 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include <Pothos/Framework.hpp>
+#include <Poco/Types.h>
+#include <Poco/JSON/Parser.h>
+#include <Poco/JSON/Array.h>
+#include <iostream>
+#include <complex>
+
+/***********************************************************************
+ * |PothosDoc Add
+ *
+ * Add elements across multiple input ports to produce a stream of outputs.
+ *
+ * out[n] = in0[n] + in1[n] + ... + in_last[n]
+ *
+ * |category /Arithmetic
+ * |keywords math add arithmetic
+ *
+ * |param dtype[Data Type] The datatype used in the arithmetic.
+ * |option [Complex128] "complex128"
+ * |option [Float64] "float64"
+ * |option [Complex64] "complex64"
+ * |option [Float32] "float32"
+ * |option [Complex Int64] "int64, 2"
+ * |option [Int64] "int64"
+ * |option [Complex Int32] "int32, 2"
+ * |option [Int32] "int32"
+ * |option [Complex Int16] "int16, 2"
+ * |option [Int16] "int16"
+ * |option [Complex Int8] "int8, 2"
+ * |option [Int8] "int8"
+ * |preview disable
+ *
+ * |param preload The number of elements to preload into each input.
+ * The value is a JSON array of integers where each element represents
+ * the number of elements to preload the port with.
+ * |default "[0, 0]"
+ * |preview disable
+ *
+ * |factory /blocks/arithmetic/add(dtype)
+ * |setter setPreload(preload)
+ **********************************************************************/
+template <typename Type>
+class Add : public Pothos::Block
+{
+public:
+    Add(void)
+    {
+        this->registerCall(POTHOS_FCN_TUPLE(Add<Type>, setPreload));
+        this->setupInput(0, typeid(Type));
+        this->setupInput(1, typeid(Type)); //TODO FIXME remove this line after: https://github.com/pothosware/pothos-library/issues/42
+        this->setupOutput(0, typeid(Type));
+    }
+
+    void setPreload(const std::string &preloadStr)
+    {
+        Poco::JSON::Parser p; p.parse(preloadStr);
+        const auto ports = p.getHandler()->asVar().extract<Poco::JSON::Array::Ptr>();
+        for (size_t i = 0; i < ports->size(); i++)
+        {
+            auto bytes = ports->getElement<int>(i)*this->input(i)->dtype().size();
+            Pothos::BufferChunk buffer(bytes);
+            this->input(i)->pushBuffer(buffer);
+        }
+    }
+
+    void work(void)
+    {
+        //access to input ports and output port
+        const std::vector<Pothos::InputPort *> &inputs = this->inputs();
+        Pothos::OutputPort *output = this->output(0);
+
+        //number of elements to work with
+        auto elems = this->workInfo().minElements;
+
+        //establish pointers to buffers
+        auto out = Pothos::BufferChunk(output->buffer()).as<Type *>();
+        auto in0 = inputs[0]->buffer().as<const Type *>();
+
+        //loop through available ports
+        for (size_t i = 1; i < inputs.size(); i++)
+        {
+            auto *inX = Pothos::BufferChunk(inputs[i]->buffer()).as<const Type *>();
+            for (size_t n = 0; n < elems; n++) //loop through elements
+            {
+                out[n] = inX[n] + in0[n];
+            }
+            in0 = out; //adding to output array next loop
+            inputs[i]->consume(elems); //consume on ith input port
+        }
+
+        //produce and consume on 0th ports
+        inputs[0]->consume(elems);
+        output->produce(elems);
+    }
+};
+
+/***********************************************************************
+ * registration
+ **********************************************************************/
+static Pothos::Block *addFactory(const Pothos::DType &dtype)
+{
+    #define ifTypeDeclareFactory(type) \
+        if (dtype == Pothos::DType(typeid(type))) return new Add<type>(); \
+        if (dtype == Pothos::DType(typeid(type), Pothos::DType::Shape(1, 2))) return new Add<std::complex<type>>();
+    ifTypeDeclareFactory(double);
+    ifTypeDeclareFactory(float);
+    ifTypeDeclareFactory(Poco::Int64);
+    ifTypeDeclareFactory(Poco::Int32);
+    ifTypeDeclareFactory(Poco::Int16);
+    ifTypeDeclareFactory(Poco::Int8);
+    throw Pothos::InvalidArgumentException("addFactory("+dtype.toString()+")", "unsupported type");
+}
+
+static Pothos::BlockRegistry registerAdd(
+    "/blocks/arithmetic/add", &addFactory);
