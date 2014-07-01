8be9b0b6ee24e00be4dcd0b2f9108b81e9841d55
Josh Blum <josh@joshknows.com>
2014-06-30 20:44:37 -0400

    pothos-gui: compilation fix for api change

--- GraphEditor/ExecutionEngine.cpp
+++ GraphEditor/ExecutionEngine.cpp
@@ -51,7 +51,7 @@ Pothos::Proxy graphBlockToProxyBlock(GraphBlock *block)
 
     //create the block
     auto proxyBlock = registry.getHandle()->call(block->getBlockDescPath(), ctorArgs.data(), ctorArgs.size());
-    proxyBlock.call("setName", block->getId().toStdString());
+    proxyBlock.callVoid("setName", block->getId().toStdString());
 
     //make the calls
     for (auto call : *blockDesc->getArray("calls"))
