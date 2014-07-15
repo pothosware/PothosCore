077e462d6e68987d6bca30288ceec61ac1cb2b1a
Josh Blum <josh@joshknows.com>
2014-07-14 22:18:24 -0700

    pothos-gui: filling in the affinity panel

--- pothos-gui/AffinityPanel.cpp
+++ pothos-gui/AffinityPanel.cpp
@@ -5,14 +5,16 @@
 #include <QVBoxLayout>
 #include <QHBoxLayout>
 #include <QFormLayout>
-#include <QLineEdit>
+#include <QSpinBox>
 #include <QPushButton>
 #include <QComboBox>
+#include <QLineEdit>
 #include <QToolTip>
-#include <QLabel>
-#include <QSlider>
 #include <QGroupBox>
 #include <QScrollArea>
+#include <iostream>
+
+static const int ARBITRARY_MAX_THREADS = 4096;
 
 class AffinityZoneEditor : public QGroupBox
 {
@@ -21,11 +23,15 @@ public:
     AffinityZoneEditor(const QString &zoneName, QWidget *parent):
         QGroupBox(zoneName, parent),
         _remoteNodesBox(new QComboBox(this)),
-        _prioritySlider(new QSlider(Qt::Horizontal, this)),
-        _priorityEdit(new QLineEdit(this)),
+        _numThreadsSpin(new QSpinBox(this)),
+        _prioritySpin(new QSpinBox(this)),
         _affinityModeBox(new QComboBox(this)),
         _removeButton(new QPushButton(makeIconFromTheme("list-remove"), tr("Remove"), this))
     {
+        //bold title
+        this->setStyleSheet("QGroupBox{font-weight: bold;}");
+
+        //setup layout
         auto formLayout = new QFormLayout(this);
         this->setLayout(formLayout);
 
@@ -33,21 +39,30 @@ public:
         {
             formLayout->addRow(tr("Remote Node"), _remoteNodesBox);
             _remoteNodesBox->addItems(getRemoteNodeUris());
+            connect(_remoteNodesBox, SIGNAL(currentIndexChanged(int)), this, SLOT(somethingChanged(int)));
+        }
+
+        //num threads
+        {
+            formLayout->addRow(tr("Num Threads"), _numThreadsSpin);
+            _numThreadsSpin->setRange(1, ARBITRARY_MAX_THREADS);
+            connect(_numThreadsSpin, SIGNAL(editingFinished(void)), this, SLOT(somethingChanged()));
         }
 
         //priority selection
         {
-            auto hbox = new QHBoxLayout();
-            hbox->addWidget(_prioritySlider);
-            hbox->addWidget(_priorityEdit);
-            formLayout->addRow(tr("Process Priority"), hbox);
-            connect(_prioritySlider, SIGNAL(sliderMoved(int)), this, SLOT(handlePrioritySlider(int)));
-            connect(_priorityEdit, SIGNAL(returnPressed(void)), this, SLOT(handlePriorityEdit(void)));
+            formLayout->addRow(tr("Process Priority %"), _prioritySpin);
+            _prioritySpin->setRange(-100, +100);
+            connect(_prioritySpin, SIGNAL(editingFinished(void)), this, SLOT(somethingChanged()));
         }
 
         //affinity mode
         {
             formLayout->addRow(tr("Affinity Mode"), _affinityModeBox);
+            _affinityModeBox->addItem(tr("Use all resources"), "ALL");
+            _affinityModeBox->addItem(tr("Specify individual CPUs"), "CPU");
+            _affinityModeBox->addItem(tr("Specify NUMA nodes"), "NUMA");
+            connect(_affinityModeBox, SIGNAL(currentIndexChanged(int)), this, SLOT(somethingChanged(int)));
         }
 
         //remove button
@@ -63,20 +78,21 @@ private slots:
         emit deleteLater();
     }
 
-    void handlePrioritySlider(int value)
+    void somethingChanged(int)
     {
-        
+        this->somethingChanged();
     }
 
-    void handlePriorityEdit(void)
+    void somethingChanged(void)
     {
-        
+        std::cout << " 	editingFinished?\n";
     }
 
 private:
+
     QComboBox *_remoteNodesBox;
-    QSlider *_prioritySlider;
-    QLineEdit *_priorityEdit;
+    QSpinBox *_numThreadsSpin;
+    QSpinBox *_prioritySpin;
     QComboBox *_affinityModeBox;
     QPushButton *_removeButton;
 };
@@ -113,6 +129,7 @@ public:
         //editors area
         {
             mainLayout->addWidget(_editorsScroll);
+            _editorsScroll->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
             _editorsScroll->setWidgetResizable(true);
             _editorsScroll->setWidget(new QWidget(_editorsScroll));
             _editorsScroll->widget()->setLayout(_editorsLayout);
@@ -135,18 +152,28 @@ private slots:
 
 private:
 
+    void ensureDefault(void)
+    {
+        //FIXME not this way
+        _editorsLayout->addWidget(new AffinityZoneEditor("default", this));
+    }
+
     void initAffinityZoneEditors(void)
     {
-        
+        //TODO restore from save
+        this->ensureDefault();
     }
 
     void saveAffinityZoneEditorsState(void)
     {
-        
+        //TODO save it
+        this->ensureDefault();
     }
 
     void handleAffinityZoneEditor(QObject *)
     {
+        //TODO maybe something other than destroyed signal to record this
+        //objects are always destroyed
         this->saveAffinityZoneEditorsState();
     }
 
