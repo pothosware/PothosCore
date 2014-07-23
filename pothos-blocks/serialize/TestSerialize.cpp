92553329a20740ea455b1f7ee205baae15e1f6aa
Josh Blum <josh@joshknows.com>
2014-07-23 10:14:42 -0700

    pothos-blocks: serializer unit test with test plan

--- pothos-blocks/serialize/TestSerialize.cpp
+++ pothos-blocks/serialize/TestSerialize.cpp
@@ -1,4 +1,38 @@
 // Copyright (c) 2014-2014 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
+#include <Pothos/Testing.hpp>
 #include <Pothos/Framework.hpp>
+#include <Pothos/Proxy.hpp>
+#include <Poco/JSON/Object.h>
+#include <iostream>
+
+POTHOS_TEST_BLOCK("/blocks/tests", test_serializer_blocks)
+{
+    auto env = Pothos::ProxyEnvironment::make("managed");
+    auto registry = env->findProxy("Pothos/BlockRegistry");
+    auto feeder = registry.callProxy("/blocks/feeder_source", "int");
+    auto collector = registry.callProxy("/blocks/collector_sink", "int");
+
+    auto serializer = registry.callProxy("/blocks/serializer");
+    auto deserializer = registry.callProxy("/blocks/deserializer");
+
+    //create a test plan
+    Poco::JSON::Object::Ptr testPlan(new Poco::JSON::Object());
+    testPlan->set("enableBuffers", true);
+    testPlan->set("enableLabels", true);
+    testPlan->set("enableMessages", true);
+    auto expected = feeder.callProxy("feedTestPlan", testPlan);
+
+    //run the topology
+    {
+        Pothos::Topology topology;
+        topology.connect(feeder, 0, serializer, 0);
+        topology.connect(serializer, 0, deserializer, 0);
+        topology.connect(deserializer, 0, collector, 0);
+        topology.commit();
+        POTHOS_TEST_TRUE(topology.waitInactive());
+    }
+
+    collector.callVoid("verifyTestPlan", expected);
+}
