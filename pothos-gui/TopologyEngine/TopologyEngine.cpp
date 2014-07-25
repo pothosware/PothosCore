030b3f0bba5b150612234b92faebfe6de40f455e
Josh Blum <josh@joshknows.com>
2014-07-25 13:46:04 -0700

    pothos-gui: topology engine sets the thread pool

--- pothos-gui/TopologyEngine/TopologyEngine.cpp
+++ pothos-gui/TopologyEngine/TopologyEngine.cpp
@@ -27,7 +27,12 @@ void TopologyEngine::commitUpdate(const GraphObjectList &graphObjects)
     {
         auto block = dynamic_cast<GraphBlock *>(obj);
         if (block == nullptr) continue;
-        idToBlock[block->getId().toStdString()] = block->getBlockEval().callProxy("getProxyBlock");
+        auto proxyBlock = block->getBlockEval().callProxy("getProxyBlock");
+        idToBlock[block->getId().toStdString()] = proxyBlock;
+
+        //set the thread pool on the proxy block
+        auto threadPool = this->getThreadPoolFromZone(block->getAffinityZone());
+        if (threadPool) proxyBlock.callVoid("setThreadPool", threadPool);
     }
 
     //make all of the connections
@@ -52,7 +57,7 @@ Pothos::ProxyEnvironment::Sptr TopologyEngine::getEnvironmentFromZone(const QStr
     auto processName = config?config->getValue<std::string>("processName"):"";
 
     //find if the environment already exists
-    auto &processToServer = _uriToProcessToServer[hostUri];
+    auto &processToServer = _uriToProcessToServerHandle[hostUri];
     if (processToServer.find(processName) == processToServer.end())
     {
         //talk to the server and spawn a new process with a new server
@@ -62,9 +67,48 @@ Pothos::ProxyEnvironment::Sptr TopologyEngine::getEnvironmentFromZone(const QStr
     }
 
     //connect to the new server and make the communication environment
-    auto serverHandle = _uriToProcessToServer.at(hostUri).at(processName);
+    auto serverHandle = _uriToProcessToServerHandle.at(hostUri).at(processName);
     auto actualPort = serverHandle.call<std::string>("getActualPort");
     Poco::URI newHostUri(hostUri);
     newHostUri.setPort(Poco::NumberParser::parseUnsigned(actualPort));
     return Pothos::RemoteClient(newHostUri.toString()).makeEnvironment("managed");
+}
+
+Pothos::Proxy TopologyEngine::getThreadPoolFromZone(const QString &zone)
+{
+    //make the thread pool if DNE
+    if (_zoneToThreadPool.find(zone) == _zoneToThreadPool.end())
+    {
+        auto env = this->getEnvironmentFromZone(zone);
+        auto dock = dynamic_cast<AffinityZonesDock *>(getObjectMap()["affinityZonesDock"]);
+        assert(dock != nullptr);
+        auto config = dock->zoneToConfig(zone);
+        if (not config) return Pothos::Proxy(); //invalid
+
+        //load the args
+        Pothos::ThreadPoolArgs args;
+        if (config->has("numThreads"))
+        {
+            args.numThreads = config->getValue<int>("numThreads");
+        }
+        if (config->has("priority"))
+        {
+            args.priority = config->getValue<double>("priority");
+        }
+        if (config->has("affinityMode") and config->has("affinityMask"))
+        {
+            args.affinityMode = config->getValue<std::string>("affinityMode");
+            auto mask = config->getArray("affinityMask");
+            for (size_t i = 0; i < mask->size(); i++) args.affinity.push_back(mask->getElement<int>(i));
+        }
+        if (config->has("yieldMode"))
+        {
+            args.yieldMode = config->getValue<std::string>("yieldMode");
+        }
+
+        //create the thread pool
+        _zoneToThreadPool[zone] = env->findProxy("Pothos/ThreadPool").callProxy("new", args);
+    }
+
+    return _zoneToThreadPool.at(zone);
 }
--- pothos-gui/TopologyEngine/TopologyEngine.hpp
+++ pothos-gui/TopologyEngine/TopologyEngine.hpp
@@ -23,14 +23,24 @@ class TopologyEngine : public QObject
     Q_OBJECT
 public:
     TopologyEngine(QObject *parent);
+    ~TopologyEngine(void)
+    {
+        this->clear();
+    }
 
     //! Commit and run the topology
     void commitUpdate(const GraphObjectList &graphObjects);
 
     /*!
-     * Query the affinity zone from the cache
+     * Get a proxy environment handle given a zone.
      */
     Pothos::ProxyEnvironment::Sptr getEnvironmentFromZone(const QString &zone);
+
+    /*!
+     * Get a thread pool handle given a zone.
+     * The result is null for an invalid zone.
+     */
+    Pothos::Proxy getThreadPoolFromZone(const QString &zone);
 
     Pothos::Topology &getTopology(void)
     {
@@ -45,7 +55,9 @@ public:
 
 private:
     //! A host URI to a map of process names to server handles
-    std::map<std::string, std::map<std::string, Pothos::Proxy>> _uriToProcessToServer;
+    std::map<std::string, std::map<std::string, Pothos::Proxy>> _uriToProcessToServerHandle;
+
+    std::map<QString, Pothos::Proxy> _zoneToThreadPool;
 
     static std::vector<ConnectionInfo> getConnectionInfo(const GraphObjectList &graphObjects);
 
