822f7ec1ce4f077bc78def7ebda3ec3d3e680b00
Josh Blum <josh@joshknows.com>
2014-08-01 00:24:38 -0700

    pothos-gui: filling in connections properties

--- pothos-gui/PropertiesPanel/ConnectionPropertiesPanel.cpp
+++ pothos-gui/PropertiesPanel/ConnectionPropertiesPanel.cpp
@@ -1,25 +1,32 @@
 // Copyright (c) 2014-2014 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
+#include "PothosGuiUtils.hpp" //make icon from theme
 #include "PropertiesPanel/ConnectionPropertiesPanel.hpp"
 #include "GraphObjects/GraphConnection.hpp"
 #include "GraphObjects/GraphBlock.hpp"
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QTreeWidget>
+#include <QPushButton>
 #include <QLabel>
 
 ConnectionPropertiesPanel::ConnectionPropertiesPanel(GraphConnection *conn, QWidget *parent):
     QWidget(parent),
-    _conn(conn)
+    _conn(conn),
+    _isSlot(_conn->getInputEndpoint().getConnectableAttrs().direction == GRAPH_CONN_SLOT),
+    _isSignal(_conn->getOutputEndpoint().getConnectableAttrs().direction == GRAPH_CONN_SIGNAL),
+    _connectButton(nullptr),
+    _removeButton(nullptr),
+    _inputListWidget(nullptr),
+    _outputListWidget(nullptr),
+    _connectionsListWidget(nullptr)
 {
     //master layout for this widget
     auto layout = new QVBoxLayout(this);
 
     auto inputEp = _conn->getInputEndpoint();
-    auto isSlot = (inputEp.getConnectableAttrs().direction == GRAPH_CONN_SLOT);
     auto outputEp = _conn->getOutputEndpoint();
-    auto isSignal = (outputEp.getConnectableAttrs().direction == GRAPH_CONN_SIGNAL);
 
     //title
     {
@@ -34,43 +41,71 @@ ConnectionPropertiesPanel::ConnectionPropertiesPanel(GraphConnection *conn, QWid
         layout->addLayout(listWidgetLayout);
 
         //output list
-        auto outputListWidget = new QTreeWidget(this);
-        listWidgetLayout->addWidget(outputListWidget);
-        outputListWidget->setColumnCount(1);
-        outputListWidget->setHeaderLabels(QStringList(QString("%1[%2]").arg(outputEp.getObj()->getId(), outputEp.getKey().id)));
+        _outputListWidget = new QTreeWidget(this);
+        listWidgetLayout->addWidget(_outputListWidget);
+        _outputListWidget->setColumnCount(1);
+        _outputListWidget->setHeaderLabels(QStringList(QString("%1[%2]").arg(outputEp.getObj()->getId(), outputEp.getKey().id)));
+        connect(_outputListWidget, SIGNAL(itemSelectionChanged(void)), this, SLOT(handleItemSelectionChanged(void)));
 
         //populate output list
         auto outputBlock = dynamic_cast<GraphBlock *>(outputEp.getObj().data());
-        if (isSignal and outputBlock != nullptr)
+        if (_isSignal and outputBlock != nullptr)
         {
             for (const auto &port : outputBlock->getSignalPorts())
             {
-                auto item = new QTreeWidgetItem(outputListWidget, QStringList(port.getName()));
-                outputListWidget->addTopLevelItem(item);
+                auto item = new QTreeWidgetItem(_outputListWidget, QStringList(port.getName()));
+                _outputItemToKey[item] = port.getKey();
+                _outputListWidget->addTopLevelItem(item);
             }
         }
-        outputListWidget->resizeColumnToContents(0);
+        _outputListWidget->resizeColumnToContents(0);
 
         //input list
-        auto inputListWidget = new QTreeWidget(this);
-        listWidgetLayout->addWidget(inputListWidget);
-        inputListWidget->setColumnCount(1);
-        inputListWidget->setHeaderLabels(QStringList(QString("%1[%2]").arg(inputEp.getObj()->getId(), inputEp.getKey().id)));
+        _inputListWidget = new QTreeWidget(this);
+        listWidgetLayout->addWidget(_inputListWidget);
+        _inputListWidget->setColumnCount(1);
+        _inputListWidget->setHeaderLabels(QStringList(QString("%1[%2]").arg(inputEp.getObj()->getId(), inputEp.getKey().id)));
+        connect(_inputListWidget, SIGNAL(itemSelectionChanged(void)), this, SLOT(handleItemSelectionChanged(void)));
 
         //populate input list
         auto inputBlock = dynamic_cast<GraphBlock *>(inputEp.getObj().data());
-        if (isSlot and inputBlock != nullptr)
+        if (_isSlot and inputBlock != nullptr)
         {
             for (const auto &port : inputBlock->getSlotPorts())
             {
-                auto item = new QTreeWidgetItem(inputListWidget, QStringList(port.getName()));
-                inputListWidget->addTopLevelItem(item);
+                auto item = new QTreeWidgetItem(_inputListWidget, QStringList(port.getName()));
+                _inputItemToKey[item] = port.getKey();
+                _inputListWidget->addTopLevelItem(item);
             }
         }
-        inputListWidget->resizeColumnToContents(0);
+        _inputListWidget->resizeColumnToContents(0);
+    }
+
+    //connect button
+    {
+        _connectButton = new QPushButton(makeIconFromTheme("list-add"), tr("Create connection"), this);
+        connect(_connectButton, SIGNAL(pressed(void)), this, SLOT(handleCreateConnection(void)));
+        layout->addWidget(_connectButton);
+    }
+
+    //existing connections
+    {
+        _connectionsListWidget = new QTreeWidget(this);
+        layout->addWidget(_connectionsListWidget);
+        _connectionsListWidget->setColumnCount(1);
+        _connectionsListWidget->setHeaderLabels(QStringList(tr("Signal slot connections")));
+        connect(_connectionsListWidget, SIGNAL(itemSelectionChanged(void)), this, SLOT(handleItemSelectionChanged(void)));
+    }
+
+    //remove button
+    {
+        _removeButton = new QPushButton(makeIconFromTheme("list-remove"), tr("Remove connection"), this);
+        connect(_removeButton, SIGNAL(pressed(void)), this, SLOT(handleRemoveConnection(void)));
+        layout->addWidget(_removeButton);
     }
 
     connect(_conn, SIGNAL(destroyed(QObject*)), this, SLOT(handleConnectionDestroyed(QObject*)));
+    this->handleItemSelectionChanged(); //init state
 }
 
 void ConnectionPropertiesPanel::handleConnectionDestroyed(QObject *)
@@ -84,4 +119,42 @@ void ConnectionPropertiesPanel::handleCancel(void)
 void ConnectionPropertiesPanel::handleCommit(void)
 {
     
+}
+
+void ConnectionPropertiesPanel::handleItemSelectionChanged(void)
+{
+    auto signalItemsSelected = _outputListWidget->selectedItems();
+    auto slotItemsSelected = _inputListWidget->selectedItems();
+    _connectButton->setEnabled(not signalItemsSelected.isEmpty() and not slotItemsSelected.isEmpty());
+    _removeButton->setEnabled(not _connectionsListWidget->selectedItems().isEmpty());
+}
+
+void ConnectionPropertiesPanel::handleCreateConnection(void)
+{
+    for (auto signalItem : _outputListWidget->selectedItems())
+    {
+        signalItem->setSelected(false);
+        for (auto slotItem : _inputListWidget->selectedItems())
+        {
+            slotItem->setSelected(false);
+            auto pair = std::make_pair(
+                _outputItemToKey.at(signalItem),
+                _inputItemToKey.at(slotItem));
+            if (this->getKeyPairs().count(pair) != 0) continue;
+            auto item = new QTreeWidgetItem(_connectionsListWidget, QStringList(QString("%1 -> %2")
+                .arg(signalItem->text(0))
+                .arg(slotItem->text(0))));
+            _connItemToKeyPair[item] = pair;
+            _connectionsListWidget->addTopLevelItem(item);
+        }
+    }
+}
+
+void ConnectionPropertiesPanel::handleRemoveConnection(void)
+{
+    for (auto item : _connectionsListWidget->selectedItems())
+    {
+        _connItemToKeyPair.erase(item);
+        delete item;
+    }
 }
--- pothos-gui/PropertiesPanel/ConnectionPropertiesPanel.hpp
+++ pothos-gui/PropertiesPanel/ConnectionPropertiesPanel.hpp
@@ -5,8 +5,14 @@
 #include <Pothos/Config.hpp>
 #include <QWidget>
 #include <QPointer>
+#include <QString>
+#include <set>
+#include <map>
 
 class GraphConnection;
+class QPushButton;
+class QTreeWidgetItem;
+class QTreeWidget;
 
 class ConnectionPropertiesPanel : public QWidget
 {
@@ -21,7 +27,26 @@ public slots:
 private slots:
 
     void handleConnectionDestroyed(QObject *);
+    void handleItemSelectionChanged(void);
+    void handleCreateConnection(void);
+    void handleRemoveConnection(void);
 
 private:
     QPointer<GraphConnection> _conn;
+    const bool _isSlot;
+    const bool _isSignal;
+    QPushButton *_connectButton;
+    QPushButton *_removeButton;
+    QTreeWidget *_inputListWidget;
+    QTreeWidget *_outputListWidget;
+    QTreeWidget *_connectionsListWidget;
+    std::map<QTreeWidgetItem *, QString> _inputItemToKey;
+    std::map<QTreeWidgetItem *, QString> _outputItemToKey;
+    std::map<QTreeWidgetItem *, std::pair<QString, QString>> _connItemToKeyPair;
+    std::set<std::pair<QString, QString>> getKeyPairs(void) const
+    {
+        std::set<std::pair<QString, QString>> out;
+        for (const auto &it : _connItemToKeyPair) out.insert(it.second);
+        return out;
+    }
 };
