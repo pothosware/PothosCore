34689d12fc81645a9864f5f2e1af64673fafc76c
Josh Blum <josh@joshknows.com>
2014-08-01 17:18:38 -0700

    pothos-library: cache the safe load results

--- pothos-library/lib/Plugin/Module.cpp
+++ pothos-library/lib/Plugin/Module.cpp
@@ -10,8 +10,6 @@
 #include <Poco/Format.h>
 #include <mutex>
 #include <Poco/SingletonHolder.h>
-#include <Poco/Process.h>
-#include <Poco/Pipe.h>
 
 /***********************************************************************
  * Locking for plugin attach helper
@@ -66,38 +64,6 @@ Pothos::PluginModule::PluginModule(const std::string &path):
     {
         throw PluginModuleError("Pothos::PluginModule("+path+")", ex.displayText());
     }
-}
-
-Pothos::PluginModule Pothos::PluginModule::safeLoad(const std::string &path)
-{
-    const int success = 200;
-
-    //create args
-    Poco::Process::Args args;
-    args.push_back("--load-module");
-    args.push_back("\""+path+"\""); //add quotes for paths with spaces
-    args.push_back("--success-code");
-    args.push_back(std::to_string(success));
-
-    //launch
-    Poco::Pipe outPipe, errPipe;
-    Poco::Process::Env env;
-    Poco::ProcessHandle ph(Poco::Process::launch(
-        Pothos::System::getPothosUtilExecutablePath(),
-        args, nullptr, &outPipe, &errPipe, env));
-
-    //close pipes to not overfill and backup
-    outPipe.close();
-    errPipe.close();
-
-    //wait, check error condition, and throw
-    if (ph.wait() != success)
-    {
-        throw Pothos::PluginModuleError("Pothos::PluginModule("+path+")", "failed safe load");
-    }
-
-    //it was safe, load into this process now
-    return PluginModule(path);
 }
 
 std::string Pothos::PluginModule::getFilePath(void) const
