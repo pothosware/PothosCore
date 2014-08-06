42b9fb102f8caaf002568dfbac14959fb214d209
Josh Blum <josh@joshknows.com>
2014-08-05 23:16:43 -0700

    pothos-library: added builtin conversions for object -> proxy containers

--- pothos-library/lib/Object/Builtin/ConvertContainers.cpp
+++ pothos-library/lib/Object/Builtin/ConvertContainers.cpp
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #include <Pothos/Object/Containers.hpp>
-#include <Pothos/Proxy/Containers.hpp>
+#include <Pothos/Proxy.hpp>
 #include <Pothos/Plugin.hpp>
 #include <Pothos/Callable.hpp>
 
@@ -40,11 +40,50 @@ static Pothos::ObjectMap convertProxyMapToObjectMap(const Pothos::ProxyMap &m)
     return objMap;
 }
 
-//TODO convert in the other direction
+static Pothos::ProxyVector convertObjectVectorToProxyVector(const Pothos::ObjectVector &v)
+{
+    auto env = Pothos::ProxyEnvironment::make("managed");
+    Pothos::ProxyVector pVec;
+    for (const auto &elem : v)
+    {
+        auto o = env->convertObjectToProxy(elem);
+        pVec.push_back(o);
+    }
+    return pVec;
+}
+
+static Pothos::ProxySet convertObjectSetToProxySet(const Pothos::ObjectSet &s)
+{
+    auto env = Pothos::ProxyEnvironment::make("managed");
+    Pothos::ProxySet pSet;
+    for (const auto &elem : s)
+    {
+        auto o = env->convertObjectToProxy(elem);
+        pSet.insert(o);
+    }
+    return pSet;
+}
+
+static Pothos::ProxyMap convertObjectMapToProxyMap(const Pothos::ObjectMap &m)
+{
+    auto env = Pothos::ProxyEnvironment::make("managed");
+    Pothos::ProxyMap pMap;
+    for (const auto &elem : m)
+    {
+        auto k = env->convertObjectToProxy(elem.first);
+        auto v = env->convertObjectToProxy(elem.second);
+        pMap[k] = v;
+    }
+    return pMap;
+}
 
 pothos_static_block(pothosObjectRegisterConvertContainers)
 {
-    Pothos::PluginRegistry::add("/object/convert/containers/proxy_vector_to_object_vector", Pothos::Callable(&convertProxyVectorToObjectVector));
+    Pothos::PluginRegistry::add("/object/convert/containers/proxy_vec_to_object_vec", Pothos::Callable(&convertProxyVectorToObjectVector));
     Pothos::PluginRegistry::add("/object/convert/containers/proxy_set_to_object_set", Pothos::Callable(&convertProxySetToObjectSet));
     Pothos::PluginRegistry::add("/object/convert/containers/proxy_map_to_object_map", Pothos::Callable(&convertProxyMapToObjectMap));
+
+    Pothos::PluginRegistry::add("/object/convert/containers/object_vec_to_proxy_vec", Pothos::Callable(&convertObjectVectorToProxyVector));
+    Pothos::PluginRegistry::add("/object/convert/containers/object_set_to_proxy_set", Pothos::Callable(&convertObjectSetToProxySet));
+    Pothos::PluginRegistry::add("/object/convert/containers/object_map_to_proxy_map", Pothos::Callable(&convertObjectMapToProxyMap));
 }
