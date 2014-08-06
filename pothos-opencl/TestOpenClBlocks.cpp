6f37796b9e6152cf95c70ba0fff5595c101eebab
Josh Blum <josh@joshknows.com>
2014-08-06 00:05:15 -0700

    pothos-opencl: replace JSON array with native vector

--- pothos-opencl/TestOpenClBlocks.cpp
+++ pothos-opencl/TestOpenClBlocks.cpp
@@ -36,7 +36,7 @@ POTHOS_TEST_BLOCK("/blocks/opencl/tests", test_opencl_kernel)
     auto feeder0 = registry.callProxy("/blocks/feeder_source", "float32");
     auto feeder1 = registry.callProxy("/blocks/feeder_source", "float32");
 
-    auto openClKernel = registry.callProxy("/blocks/opencl_kernel", "0:0", "[[4, 4], [4]]");
+    auto openClKernel = registry.callProxy("/blocks/opencl_kernel", "0:0", std::vector<size_t>(2, 4), std::vector<size_t>(1, 4));
     openClKernel.callVoid("setSource", "add_2x_float32", KERNEL_SOURCE);
     openClKernel.callVoid("setLocalSize", 1);
     openClKernel.callVoid("setGlobalFactor", 1.0);
@@ -83,13 +83,13 @@ POTHOS_TEST_BLOCK("/blocks/opencl/tests", test_opencl_kernel_back_to_back)
     auto feeder1 = registry.callProxy("/blocks/feeder_source", "float32");
     auto feeder2 = registry.callProxy("/blocks/feeder_source", "float32");
 
-    auto openClKernel0 = registry.callProxy("/blocks/opencl_kernel", "0:0", "[[4, 4], [4]]");
+    auto openClKernel0 = registry.callProxy("/blocks/opencl_kernel", "0:0", std::vector<size_t>(2, 4), std::vector<size_t>(1, 4));
     openClKernel0.callVoid("setSource", "add_2x_float32", KERNEL_SOURCE);
     openClKernel0.callVoid("setLocalSize", 1);
     openClKernel0.callVoid("setGlobalFactor", 1.0);
     openClKernel0.callVoid("setProductionFactor", 1.0);
 
-    auto openClKernel1 = registry.callProxy("/blocks/opencl_kernel", "0:0", "[[4, 4], [4]]");
+    auto openClKernel1 = registry.callProxy("/blocks/opencl_kernel", "0:0", std::vector<size_t>(2, 4), std::vector<size_t>(1, 4));
     openClKernel1.callVoid("setSource", "add_2x_float32", KERNEL_SOURCE);
     openClKernel1.callVoid("setLocalSize", 1);
     openClKernel1.callVoid("setGlobalFactor", 1.0);
