da8db6e5529248694148eb8690809c577915b52c
Josh Blum <josh@joshknows.com>
2014-08-08 22:21:04 -0700

    pothos-widgets: slider callbacks + docs

--- pothos-widgets/Slider.cpp
+++ pothos-widgets/Slider.cpp
@@ -4,15 +4,48 @@
 #include <Pothos/Framework.hpp>
 #include <QSlider>
 
+/***********************************************************************
+ * |PothosDoc Slider
+ *
+ * A slider widget for graphical control of an integer value.
+ *
+ * |category /Widgets
+ * |keywords slider
+ *
+ * |param value The initial value of this slider.
+ * |default 0
+ *
+ * |param value The minimum integer value of this slider.
+ * |default 0
+ *
+ * |param value The maximum integer value of this slider.
+ * |default 100
+ *
+ * |factory /widgets/slider()
+ * |setter setValue(value)
+ * |setter setMinimum(minimum)
+ * |setter setMaximum(maximum)
+ **********************************************************************/
 class Slider : public QSlider, public Pothos::Block
 {
     Q_OBJECT
 public:
 
+    static Block *make(void)
+    {
+        return new Slider();
+    }
+
     Slider(void):
         QSlider(nullptr)
     {
         this->registerCall(POTHOS_FCN_TUPLE(Slider, getWidget));
+        this->registerCall(POTHOS_FCN_TUPLE(Slider, value));
+        this->registerCall(POTHOS_FCN_TUPLE(Slider, setValue));
+        this->registerCall(POTHOS_FCN_TUPLE(Slider, setMinimum));
+        this->registerCall(POTHOS_FCN_TUPLE(Slider, setMaximum));
+        this->registerSignal("valueChanged");
+        connect(this, SIGNAL(valueChanged(const int)), this, SLOT(handleValueChanged(const int)));
     }
 
     QWidget *getWidget(void)
@@ -20,6 +53,20 @@ public:
         return this;
     }
 
+    void activate(void)
+    {
+        //emit current value when design becomes active
+        this->emitSignal("valueChanged", this->value());
+    }
+
+private slots:
+    void handleValueChanged(const int value)
+    {
+        this->emitSignal("valueChanged", value);
+    }
 };
+
+static Pothos::BlockRegistry registerAdd(
+    "/widgets/slider", &Slider::make);
 
 #include "Slider.moc"
