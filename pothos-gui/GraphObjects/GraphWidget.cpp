7b67234472184c88809812d6d147f317e93d71f3
Josh Blum <josh@joshknows.com>
2014-08-15 11:26:14 -0700

    pothos-gui: moved GraphWidgetContainer into its own file

--- pothos-gui/GraphObjects/GraphWidget.cpp
+++ pothos-gui/GraphObjects/GraphWidget.cpp
@@ -3,6 +3,7 @@
 
 #include "GraphObjects/GraphWidget.hpp"
 #include "GraphObjects/GraphBlock.hpp"
+#include "GraphObjects/GraphWidgetContainer.hpp"
 #include "GraphEditor/Constants.hpp"
 #include "GraphEditor/GraphDraw.hpp"
 #include "GraphEditor/GraphEditor.hpp"
@@ -12,158 +13,9 @@
 #include <QPen>
 #include <QBrush>
 #include <QColor>
-#include <QSizeGrip>
-#include <QVBoxLayout>
-#include <QStaticText>
-#include <QPainter>
 #include <vector>
 #include <iostream>
 #include <cassert>
-
-/***********************************************************************
- * A resizable container with a QSizeGrip handle
- **********************************************************************/
-class MySizeGrip : public QSizeGrip
-{
-    Q_OBJECT
-public:
-    MySizeGrip(QWidget *parent):
-        QSizeGrip(parent)
-    {
-        return;
-    }
-
-signals:
-    void resized(void);
-
-protected:
-    void mousePressEvent(QMouseEvent *event)
-    {
-        if (event->button() == Qt::LeftButton)
-        {
-            _pressPos = this->pos();
-        }
-        QSizeGrip::mousePressEvent(event);
-    }
-
-    void mouseReleaseEvent(QMouseEvent *event)
-    {
-        if (event->button() == Qt::LeftButton)
-        {
-            if (_pressPos != this->pos()) emit this->resized();
-        }
-        QSizeGrip::mouseReleaseEvent(event);
-    }
-
-private:
-    QPointF _pressPos;
-};
-
-
-class ResizableWidgetContainer : public QWidget
-{
-    Q_OBJECT
-public:
-    ResizableWidgetContainer(void):
-        _layout(new QVBoxLayout(this)),
-        _grip(new MySizeGrip(this)),
-        _widget(nullptr)
-    {
-        this->setLayout(_layout);
-        _layout->setContentsMargins(QMargins(3, 3, 3, 3));
-        _layout->setSpacing(1);
-        _layout->addWidget(_grip, 0, Qt::AlignBottom | Qt::AlignRight);
-        connect(_grip, SIGNAL(resized(void)), this, SIGNAL(resized(void)));
-        _grip->hide();
-    }
-
-    ~ResizableWidgetContainer(void)
-    {
-        //remove this container as a parent to widget
-        this->setWidget(nullptr);
-    }
-
-    void setWidget(QWidget *widget)
-    {
-        //no change, just return
-        if (_widget == widget) return;
-
-        //remove old widget, dont delete it
-        if (_widget)
-        {
-            //we dont own the widget, dont delete it
-            _layout->removeWidget(_widget);
-            _widget->setParent(nullptr);
-        }
-
-        //stash new widget and add to layout
-        _widget = widget;
-        if (_widget) _layout->insertWidget(0, _widget);
-        this->setShowGrip(false);
-    }
-
-    void setGripLabel(const QString &name)
-    {
-        _gripLabel = QStaticText(QString("<b><span color='black'>%1</span></b>").arg(name.toHtmlEscaped()));
-    }
-
-signals:
-    void resized(void);
-
-protected:
-    void enterEvent(QEvent *event)
-    {
-        this->setShowGrip(true);
-        QWidget::enterEvent(event);
-    }
-
-    void leaveEvent(QEvent *event)
-    {
-        this->setShowGrip(false);
-        QWidget::leaveEvent(event);
-    }
-
-    void setShowGrip(const bool visible)
-    {
-        if (not _widget) return;
-        _widget->show(); //needs visibility to calculate size
-
-        //stash the relevant settings
-        auto oldPolicy = _widget->sizePolicy();
-        auto oldMinSize = _widget->minimumSize();
-
-        //fix the sizes so the widget wont be hurt by adjustSize
-        _widget->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-        _widget->setMinimumSize(_widget->size());
-
-        //change the visibility and adjust to the new overall size
-        _grip->setVisible(visible);
-        this->adjustSize();
-
-        //restore settings to the widget
-        _widget->setMinimumSize(oldMinSize);
-        _widget->setSizePolicy(oldPolicy);
-    }
-
-    void paintEvent(QPaintEvent *event)
-    {
-        QWidget::paintEvent(event);
-        if (not _widget) return;
-        if (not _grip->isVisible()) return;
-
-        QPainter painter(this);
-        auto panelH = this->height() - _widget->height();
-        painter.drawStaticText(QPointF((this->width()-_gripLabel.size().width())/2.,
-            _widget->height()+(panelH-_gripLabel.size().height())/2.), _gripLabel);
-        painter.end();
-    }
-
-private:
-    QStaticText _gripLabel;
-    QVBoxLayout *_layout;
-    QSizeGrip *_grip;
-    QPointer<QWidget> _widget;
-};
 
 /***********************************************************************
  * GraphWidget private container
@@ -172,7 +24,7 @@ struct GraphWidget::Impl
 {
     Impl(QGraphicsItem *parent):
         changed(true),
-        container(new ResizableWidgetContainer()),
+        container(new GraphWidgetContainer()),
         graphicsWidget(new QGraphicsProxyWidget(parent))
     {
         graphicsWidget->setWidget(container);
@@ -189,7 +41,7 @@ struct GraphWidget::Impl
 
     QRectF mainRect;
 
-    ResizableWidgetContainer *container;
+    GraphWidgetContainer *container;
     QGraphicsProxyWidget *graphicsWidget;
 };
 
@@ -318,5 +170,3 @@ void GraphWidget::deserialize(Poco::JSON::Object::Ptr obj)
 
     GraphObject::deserialize(obj);
 }
-
-#include "GraphWidget.moc"
