6b2de9867a2edb52d03677375cbb8d8cbd0f799d
Josh Blum <josh@joshknows.com>
2014-08-17 10:54:40 -0700

    pothos-library: split-up flow comprehension impls

--- pothos-library/lib/Framework/TopologyImpl.hpp
+++ pothos-library/lib/Framework/TopologyImpl.hpp
@@ -18,6 +18,7 @@ struct Pothos::Topology::Impl
     std::vector<Flow> flows;
     std::vector<Flow> activeFlatFlows;
     std::unordered_map<Flow, std::pair<Flow, Flow>> flowToNetgressCache;
+    std::vector<Flow> squashFlows(const std::vector<Flow> &);
     std::vector<Flow> createNetworkFlows(const std::vector<Flow> &);
     std::vector<Flow> rectifyDomainFlows(const std::vector<Flow> &);
     std::vector<std::string> inputPortNames;
@@ -27,6 +28,10 @@ struct Pothos::Topology::Impl
     std::map<std::string, Callable> calls;
 };
 
+
+/***********************************************************************
+ * get a unique object set given flows + excludes
+ **********************************************************************/
 inline std::vector<Pothos::Proxy> getObjSetFromFlowList(const std::vector<Flow> &flows, const std::vector<Flow> &excludes = std::vector<Flow>())
 {
     std::map<std::string, Pothos::Proxy> uniques;
@@ -43,4 +48,66 @@ inline std::vector<Pothos::Proxy> getObjSetFromFlowList(const std::vector<Flow> 
     std::vector<Pothos::Proxy> set;
     for (const auto &pair : uniques) set.push_back(pair.second);
     return set;
+}
+
+/***********************************************************************
+ * Make a proxy if not already
+ **********************************************************************/
+inline Pothos::Proxy getProxy(const Pothos::Object &o)
+{
+    if (o.type() == typeid(Pothos::Proxy)) return o.extract<Pothos::Proxy>();
+    return Pothos::ProxyEnvironment::make("managed")->convertObjectToProxy(o);
+}
+
+/***********************************************************************
+ * Avoid taking copies of self
+ **********************************************************************/
+inline Pothos::Proxy getInternalObject(const Pothos::Object &o, const Pothos::Topology &t)
+{
+    auto proxy = getProxy(o);
+    if (proxy.call<std::string>("uid") == t.uid()) return Pothos::Proxy();
+    return proxy;
+}
+
+/***********************************************************************
+ * Want the internal block - for language bindings
+ **********************************************************************/
+inline Pothos::Proxy getInternalBlock(const Pothos::Proxy &block)
+{
+    Pothos::Proxy internal;
+    try
+    {
+        internal = block.callProxy("getInternalBlock");
+    }
+    catch (const Pothos::Exception &)
+    {
+        internal = block;
+    }
+    assert(internal.getEnvironment()->getName() == "managed");
+    return internal;
+}
+
+/***********************************************************************
+ * Want something to make connectable calls on
+ **********************************************************************/
+inline Pothos::Proxy getConnectable(const Pothos::Object &o)
+{
+    auto proxy = getProxy(o);
+    return getInternalBlock(proxy);
+}
+
+/***********************************************************************
+ * can this object be connected with -- can we get a uid?
+ **********************************************************************/
+inline bool checkObj(const Pothos::Object &o)
+{
+    try
+    {
+        getProxy(o).call<std::string>("uid");
+    }
+    catch(...)
+    {
+        return false;
+    }
+    return true;
 }
---
+++ pothos-library/lib/Framework/TopologySquashFlows.cpp
@@ -0,0 +1,92 @@
+// Copyright (c) 2014-2014 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include "Framework/TopologyImpl.hpp"
+
+/***********************************************************************
+ * helpers to deal with recursive topology comprehension
+ **********************************************************************/
+static std::vector<Port> resolvePorts(const Port &port, const bool isSource);
+
+std::vector<Port> resolvePortsFromTopology(const Pothos::Topology &t, const std::string &portName, const bool isSource)
+{
+    std::vector<Port> ports;
+    for (const auto &flow : t._impl->flows)
+    {
+        //recurse through sub topology flows
+        std::vector<Port> subPorts;
+        if (isSource and flow.dst.name == portName and not flow.dst.obj)
+        {
+            subPorts = resolvePorts(flow.src, isSource);
+        }
+        if (not isSource and flow.src.name == portName and not flow.src.obj)
+        {
+            subPorts = resolvePorts(flow.dst, isSource);
+        }
+        ports.insert(ports.end(), subPorts.begin(), subPorts.end());
+    }
+    return ports;
+}
+
+static std::vector<Port> resolvePorts(const Port &port, const bool isSource)
+{
+    std::vector<Port> ports;
+
+    //resolve ports connected to the topology
+    try
+    {
+        auto subPorts = port.obj.callProxy("resolvePorts", port.name, isSource);
+        for (size_t i = 0; i < subPorts.call<size_t>("size"); i++)
+        {
+            auto portProxy = subPorts.callProxy("at", i);
+            Port port;
+            port.name = portProxy.call<std::string>("get:name");
+            port.obj = portProxy.callProxy("get:obj");
+            ports.push_back(port);
+        }
+    }
+    catch (const Pothos::Exception &)
+    {
+        //its just a block, no ports to resolve
+        ports.push_back(port);
+    }
+
+    return ports;
+}
+
+std::vector<Flow> Pothos::Topology::Impl::squashFlows(const std::vector<Flow> &flows)
+{
+    std::vector<Flow> flatFlows;
+
+    for (const auto &flow : flows)
+    {
+        //ignore external flows
+        if (not flow.src.obj) continue;
+        if (not flow.dst.obj) continue;
+
+        //gather a list of sources and destinations on either end of this flow
+        std::vector<Port> srcs = resolvePorts(flow.src, true);
+        std::vector<Port> dsts = resolvePorts(flow.dst, false);
+
+        //all combinations of srcs + dsts are flows
+        for (const auto &src : srcs)
+        {
+            for (const auto &dst : dsts)
+            {
+                Flow flatFlow;
+                flatFlow.src = src;
+                flatFlow.dst = dst;
+                flatFlows.push_back(flatFlow);
+            }
+        }
+    }
+
+    //only store the actual blocks
+    for (auto &flow : flatFlows)
+    {
+        flow.src.obj = getInternalBlock(flow.src.obj);
+        flow.dst.obj = getInternalBlock(flow.dst.obj);
+    }
+
+    return flatFlows;
+}
