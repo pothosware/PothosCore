3ddc651da790fa87ad258a74fff2ed64e162c33f
Josh Blum <josh@joshknows.com>
2014-09-12 13:22:36 -0400

    pothos-python: work on example build project

--- pothos-python/cmake/Modules/PothosPythonUtil.cmake
+++ pothos-python/cmake/Modules/PothosPythonUtil.cmake
@@ -9,6 +9,10 @@ include(${POTHOS_MODULE_PATH}/PothosUtil.cmake)
 ## POTHOS_PYTHON_UTIL - build and install python modules for Pothos
 ##
 ## TARGET - the name of the python module to build
+## SOURCES - 
+## DESTINATION - 
+## DOC_SOURCES - 
+## ENABLE_DOCS - 
 ##
 ## All other arguments are passed directly to the POTHOS_MODULE_UTIL()
 ## See documentation for POTHOS_MODULE_UTIL() in PothosUtil.cmake
@@ -16,10 +20,28 @@ include(${POTHOS_MODULE_PATH}/PothosUtil.cmake)
 function(POTHOS_PYTHON_UTIL)
 
     include(CMakeParseArguments)
-    CMAKE_PARSE_ARGUMENTS(POTHOS_MODULE_UTIL "ENABLE_DOCS" "TARGET;DESTINATION" "SOURCES;LIBRARIES;DOC_SOURCES" ${ARGN})
+    CMAKE_PARSE_ARGUMENTS(POTHOS_PYTHON_UTIL "ENABLE_DOCS" "TARGET;DESTINATION" "SOURCES;DOC_SOURCES" ${ARGN})
+    
+    #install python sources
+    if (POTHOS_PYTHON_UTIL_SOURCES)
+        install(
+            FILES ${${POTHOS_PYTHON_UTIL_SOURCES}}
+            DESTINATION ${POTHOS_PYTHON_UTIL_DESTINATION}
+        )
+    endif()
 
-    #generate block factories
-
-    POTHOS_MODULE_UTIL(${ARGN})
+    #build the module
+    if (POTHOS_PYTHON_UTIL_ENABLE_DOCS)
+        set(POTHOS_PYTHON_UTIL_ENABLE_DOCS "ENABLE_DOCS")
+    else()
+        unset(POTHOS_PYTHON_UTIL_ENABLE_DOCS)
+    endif()
+    POTHOS_MODULE_UTIL(
+        TARGET ${POTHOS_PYTHON_UTIL_TARGET}
+        DESTINATION ${POTHOS_PYTHON_UTIL_DESTINATION}
+        SOURCES ${POTHOS_PYTHON_UTIL_SOURCES}
+        DOC_SOURCES ${POTHOS_PYTHON_UTIL_DOC_SOURCES}
+        ${POTHOS_PYTHON_UTIL_ENABLE_DOCS}
+    )
 
 endfunction(POTHOS_PYTHON_UTIL)
---
+++ pothos-python/example/TestBlocks.py
@@ -0,0 +1,95 @@
+# Copyright (c) 2014 Josh Blum
+# SPDX-License-Identifier: BSL-1.0
+
+import Pothos
+
+"""/*
+|PothosDoc Forwarder (python)
+
+The Python forwarder block forwards all data
+from input port 0 to the output port 0.
+This block is mainly used for testing purposes.
+
+|category /Misc
+|keywords forwarder
+
+|param dtype[Data Type] The input and output data type.
+|default "float32"
+
+|factory /python/forwarder(dtype)
+*/"""
+class Forwarder(Pothos.Block):
+    def __init__(self, dtype):
+        Pothos.Block.__init__(self)
+        self.setupInput("0", dtype)
+        self.setupOutput("0", dtype)
+
+    def activate(self):
+        print('activate called')
+
+    def deactivate(self):
+        print('deactivate called')
+
+    def work(self):
+
+        if self.workInfo().minElements:
+            print('minElements %s'%self.workInfo().minElements)
+            print('minInElements %s'%self.workInfo().minInElements)
+            print('minOutElements %s'%self.workInfo().minOutElements)
+
+        #forward message
+        if self.input(0).hasMessage():
+            m = self.input(0).popMessage()
+            print('msg %s'%m)
+            self.output(0).postMessage(m)
+
+        #forward labels
+        while True:
+            loop = False
+            for l in self.input(0).labels():
+                print(l.index)
+                print(l.data)
+                self.output(0).postLabel(l)
+                self.input(0).removeLabel(l)
+                loop = True
+                break
+            if not loop: break
+
+        #forward buffer
+        if self.input(0).elements():
+            print(self.input(0).dtype())
+            print(self.input(0).buffer())
+
+            out0 = self.output(0).buffer()
+            in0 = self.input(0).buffer()
+            n = min(len(out0), len(in0))
+            out0[:n] = in0[:n]
+            self.input(0).consume(n)
+            self.output(0).produce(n)
+
+    def propagateLabels(self, input):
+        print('propagateLabels')
+        print(self.input(0).totalElements())
+        for l in input.labels():
+            print(l.index)
+            print(l.data)
+            #self.output(0).postLabel(l)
+
+class SimpleSignalEmitter(Pothos.Block):
+    def __init__(self):
+        Pothos.Block.__init__(self)
+        self.registerSignal("activateCalled")
+
+    def activate(self):
+        self.activateCalled("hello")
+
+class SimpleSlotHandler(Pothos.Block):
+    def __init__(self):
+        Pothos.Block.__init__(self)
+        self.registerSlot("activateHandler")
+
+    def activateHandler(self, word):
+        self._lastWord = word
+
+    def getLastWord(self):
+        return self._lastWord
