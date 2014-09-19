398538674a0f01771d78be152ee61b2587f8e25f
Josh Blum <josh@joshknows.com>
2014-09-19 15:42:27 -0400

    pothos-library: accumulate when requirement is larger than the actual buffer

--- pothos-library/lib/Framework/BufferAccumulator.cpp
+++ pothos-library/lib/Framework/BufferAccumulator.cpp
@@ -43,7 +43,7 @@ public:
 
 private:
     size_t _minBuffSize;
-    std::vector <Pothos::BufferChunk> _buffs;
+    std::vector<Pothos::BufferChunk> _buffs;
 };
 
 /***********************************************************************
@@ -192,9 +192,11 @@ void Pothos::BufferAccumulator::require(const size_t numBytes)
     assert(not queue.empty());
 
     //dont do anything if the buffer is large enough
-    //or the accumulator itself doesnt have enough bytes
     if (queue.front().length >= numBytes) return;
-    if (_bytesAvailable < numBytes) return;
+
+    //or the accumulator itself doesnt have enough bytes -- but can eventually have enough
+    //we deuduce this by checking if the requirement is larger than the actual buffer size
+    if (_bytesAvailable < numBytes and numBytes <= queue.front().getBuffer().getLength()) return;
 
     //Actually this is ok: assert(not _impl->inPoolBuffer);
     //The smaller pool buffer in front will be absorbed and popped.
