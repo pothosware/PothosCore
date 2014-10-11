7d7792e7db4b3ce95271c1d3edded5e2ae783d5c
Josh Blum <josh@joshknows.com>
2014-10-11 00:51:19 -0700

    pothos-sdr: filling in calls for source block

--- pothos-sdr/SDRSource.cpp
+++ pothos-sdr/SDRSource.cpp
@@ -20,25 +20,56 @@
  * A map of key/value string pairs that identifies a SDR device.
  * |default {"driver":"null", "type":"null"}
  * |preview disable
+ * |tab Streaming
  *
  * |param dtype[Data Type] The data type produced by the SDR source.
  * |widget DTypeChooser(float=1,cfloat=1,int=1,cint=1)
  * |default "complex_float32"
  * |preview disable
+ * |tab Streaming
  *
  * |param channels[Channels] A list of RX channels to stream from.
  * Each element maps a port index on the block to a device channel.
  * |default [0]
  * |preview disable
+ * |tab Streaming
  *
  * |param streamArgs[Stream Args] Additional stream arguments.
  * A map of key/value string pairs with implementation-specific meaning.
  * |default {}
  * |preview disable
+ * |tab Streaming
+ *
+ * |param sampleRate[Sample Rate] The rate of sample stream on each channel.
+ * |units Sps
+ * |default 1e6
+ * |tab Streaming
+ *
+ * |param clockRate[Clock rate] The master reference clock rate.
+ * |default 0.0
+ * |units Hz
+ * |preview disable
+ * |tab Clocking
+ *
+ * |param clockSource[Clock Source] The source of the reference clock.
+ * |default ""
+ * |preview disable
+ * |tab Clocking
+ * |widget StringEntry()
+ *
+ * |param timeSource[Time Source] The source of the hardware time.
+ * |default ""
+ * |preview disable
+ * |tab Clocking
+ * |widget StringEntry()
  *
  * |factory /sdr/source(dtype, channels)
  * |initializer setupDevice(deviceArgs)
  * |initializer setupStream(streamArgs)
+ * |setter setSampleRate(sampleRate)
+ * |setter setClockRate(clockRate)
+ * |setter setClockSource(clockSource)
+ * |setter setTimeSource(timeSource)
  **********************************************************************/
 class SDRSource : public Pothos::Block
 {
@@ -56,6 +87,14 @@ public:
         for (size_t i = 0; i < channels.size(); i++) this->setupOutput(i, dtype);
         this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, setupDevice));
         this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, setupStream));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, setSampleRate));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, getSampleRate));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, setClockRate));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, getClockRate));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, setClockSource));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, getClockSource));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, setTimeSource));
+        this->registerCall(this, POTHOS_FCN_TUPLE(SDRSource, getTimeSource));
     }
 
     ~SDRSource(void)
@@ -93,11 +132,11 @@ public:
 
     Pothos::Object opaqueCallHandler(const std::string &name, const Pothos::Object *inputArgs, const size_t numArgs)
     {
+        //try to setup the device future first
+        if (name == "setupDevice") return Pothos::Block::opaqueCallHandler(name, inputArgs, numArgs);
+
         //when ready forward the call to the handler
-        if (this->isReady() or name == "setupDevice")
-        {
-            return Pothos::Block::opaqueCallHandler(name, inputArgs, numArgs);
-        }
+        if (this->isReady()) return Pothos::Block::opaqueCallHandler(name, inputArgs, numArgs);
 
         //cache attempted settings when not ready
         const bool isSetter = (name.size() > 3 and name.substr(0, 3) == "set");
@@ -106,8 +145,59 @@ public:
         return Pothos::Object();
     }
 
+    void setSampleRate(const double rate)
+    {
+        for (const size_t chan : _channels)
+        {
+            _device->setSampleRate(SOAPY_SDR_RX, chan, rate);
+        }
+    }
+
+    double getSampleRate(void) const
+    {
+        return _device->getSampleRate(SOAPY_SDR_RX, _channels.front());
+    }
+
+    void setClockRate(const double rate)
+    {
+        if (rate == 0.0) return;
+        return _device->setMasterClockRate(rate);
+    }
+
+    double getClockRate(void) const
+    {
+        return _device->getMasterClockRate();
+    }
+
+    void setClockSource(const std::string &source)
+    {
+        if (source.empty()) return;
+        return _device->setClockSource(source);
+    }
+
+    std::string getClockSource(void) const
+    {
+        return _device->getClockSource();
+    }
+
+    void setTimeSource(const std::string &source)
+    {
+        if (source.empty()) return;
+        return _device->setTimeSource(source);
+    }
+
+    std::string getTimeSource(void) const
+    {
+        return _device->getTimeSource();
+    }
+
     void activate(void)
     {
+        if (not this->isReady())
+        {
+            throw Pothos::Exception("SDRSource::activate()", "device not ready");
+        }
+        //TODO other arguments
         _device->activateStream(_stream);
     }
 
@@ -124,6 +214,8 @@ public:
         const long timeoutUs = this->workInfo().maxTimeoutNs/1000;
         const auto &buffs = this->workInfo().outputPointers;
         const int ret = _device->readStream(_stream, buffs.data(), numElems, flags, timeNs, timeoutUs);
+
+        //TODO labels
 
         if (ret > 0) for (auto output : this->outputs()) output->produce(numElems);
         else if (ret == SOAPY_SDR_TIMEOUT) return this->yield();
