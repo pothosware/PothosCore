6c8fa94d505ec2075d5dc8c02b58aae2a52a4136
Josh Blum <josh@joshknows.com>
2014-10-16 19:00:15 -0700

    pothos: build simplifications and project config

--- pothos-library/cmake/PothosConfig.cmake
+++ pothos-library/cmake/PothosConfig.cmake
@@ -1,13 +1,72 @@
-if(DEFINED INCLUDED_FIND_POTHOS_CMAKE)
+if(DEFINED INCLUDED_POTHOS_CONFIG_CMAKE)
     return()
 endif()
-set(INCLUDED_FIND_POTHOS_CMAKE TRUE)
+set(INCLUDED_POTHOS_CONFIG_CMAKE TRUE)
 
 ########################################################################
-## Configure installation variables
+# PothosConfig - sets Pothos installation variables
+#
+# POTHOS_UTIL_EXE - the path to PothosUtil executable
+# Pothos_INCLUDE_DIRS - Pothos development includes
+# Pothos_LIBRARIES - Pothos development libraries
+# POTHOS_CMAKE_DIRECTORY - where to install CMake files
 ########################################################################
+list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 include(PothosProjectDefaults) #defines LIB_SUFFIX
-set(POTHOS_ROOT ${CMAKE_INSTALL_PREFIX}) #same as install prefix
+include(PothosUtil) #utility functions
+
+if (UNIX)
+    set(POTHOS_CMAKE_DIRECTORY share/cmake/Pothos)
+elseif (WIN32)
+    set(POTHOS_CMAKE_DIRECTORY cmake)
+endif ()
+
+########################################################################
+## it-tree build support
+########################################################################
+
+if (POTHOS_IN_TREE_SOURCE_DIR)
+
+    list(APPEND Pothos_LIBRARIES
+        Pothos
+        PothosSerialization
+        ${POCO_LIBRARIES}
+    )
+
+    list(APPEND Pothos_INCLUDE_DIRS
+        ${POTHOS_IN_TREE_SOURCE_DIR}/pothos-library/include
+        ${POTHOS_IN_TREE_SOURCE_DIR}/pothos-serialization/include
+        ${POCO_INCLUDE_DIRS}
+    )
+
+    get_target_property(POTHOS_UTIL_EXE PothosUtil LOCATION_${CMAKE_BUILD_TYPE})
+
+    if(MSVC)
+        set(built_dll_paths "%PATH%")
+        foreach(target ${Pothos_LIBRARIES})
+            get_target_property(library_location ${target} LOCATION_${CMAKE_BUILD_TYPE})
+            get_filename_component(library_location ${library_location} PATH)
+            file(TO_NATIVE_PATH ${library_location} library_location)
+            set(built_dll_paths "${library_location};${built_dll_paths}")
+        endforeach(target)
+        file(WRITE ${PROJECT_BINARY_DIR}/PothosUtil.bat
+            "set PATH=${built_dll_paths}\n"
+            "${POTHOS_UTIL_EXE} %*\n"
+        )
+        set(POTHOS_UTIL_EXE ${PROJECT_BINARY_DIR}/PothosUtil.bat)
+    endif(MSVC)
+
+    return()
+endif ()
+
+########################################################################
+## Determine root installation path
+########################################################################
+if (UNIX)
+    get_filename_component(POTHOS_ROOT "${CMAKE_CURRENT_LIST_DIR}/../../.." ABSOLUTE)
+elseif (WIN32)
+    get_filename_component(POTHOS_ROOT "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+endif ()
 
 ########################################################################
 ## locate the PothosUtil application
@@ -41,7 +100,7 @@ find_library(
 if(NOT POTHOS_LIBRARY)
     message(FATAL_ERROR "cannot find Pothos library in ${POTHOS_ROOT}/lib${LIB_SUFFIX}")
 endif()
-list(APPEND POTHOS_LIBRARIES ${POTHOS_LIBRARY})
+list(APPEND Pothos_LIBRARIES ${POTHOS_LIBRARY})
 
 ########################################################################
 ## locate the Pothos Serialization library
@@ -54,7 +113,7 @@ find_library(
 if(NOT POTHOS_LIBRARY)
     message(FATAL_ERROR "cannot find PothosSerialization library in ${POTHOS_ROOT}/lib${LIB_SUFFIX}")
 endif()
-list(APPEND POTHOS_LIBRARIES ${POTHOS_SERIALIZATION_LIBRARY})
+list(APPEND Pothos_LIBRARIES ${POTHOS_SERIALIZATION_LIBRARY})
 
 ########################################################################
 ## locate the Pothos includes
@@ -67,7 +126,7 @@ find_path(
 if(NOT POTHOS_LIBRARY)
     message(FATAL_ERROR "cannot find Pothos includes in ${POTHOS_ROOT}/include")
 endif()
-list(APPEND POTHOS_INCLUDE_DIRS ${POTHOS_INCLUDE_DIR})
+list(APPEND Pothos_INCLUDE_DIRS ${POTHOS_INCLUDE_DIR})
 
 ########################################################################
 ## locate the Poco libraries
@@ -81,5 +140,5 @@ foreach(lib Foundation Data JSON XML Util Zip Net PDF MongoDB DataSQLite)
     if(NOT POCO_LIBRARY_${lib})
         message(FATAL_ERROR "cannot find POCO_LIBRARY_${lib} library in ${POTHOS_ROOT}/lib${LIB_SUFFIX}")
     endif()
-    list(APPEND POTHOS_LIBRARIES ${POCO_LIBRARY_${lib}})
+    list(APPEND Pothos_LIBRARIES ${POCO_LIBRARY_${lib}})
 endforeach(lib)
--- pothos-library/cmake/PothosUtil.cmake
+++ pothos-library/cmake/PothosUtil.cmake
@@ -3,12 +3,6 @@ if(DEFINED INCLUDED_POTHOS_UTIL_CMAKE)
 endif()
 set(INCLUDED_POTHOS_UTIL_CMAKE TRUE)
 
-########################################################################
-# Project settings
-########################################################################
-list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
-find_package(Pothos)
-
 #recommended compiler flags (must be included at global scope)
 include(PothosStandardFlags)
 
@@ -91,9 +85,9 @@ function(POTHOS_MODULE_UTIL)
     endif()
 
     #setup module build and install rules
-    include_directories(${POTHOS_INCLUDE_DIRS})
+    include_directories(${Pothos_INCLUDE_DIRS})
     add_library(${POTHOS_MODULE_UTIL_TARGET} MODULE ${POTHOS_MODULE_UTIL_SOURCES})
-    target_link_libraries(${POTHOS_MODULE_UTIL_TARGET} ${POTHOS_LIBRARIES} ${POTHOS_MODULE_UTIL_LIBRARIES})
+    target_link_libraries(${POTHOS_MODULE_UTIL_TARGET} ${Pothos_LIBRARIES} ${POTHOS_MODULE_UTIL_LIBRARIES})
     set_target_properties(${POTHOS_MODULE_UTIL_TARGET} PROPERTIES DEBUG_POSTFIX "") #same name in debug mode
     install(
         TARGETS ${POTHOS_MODULE_UTIL_TARGET}
