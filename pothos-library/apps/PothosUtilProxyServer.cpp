d3778fa9c0591bb9bf7ae5e4e56d950c9d6d2141
Josh Blum <josh@joshknows.com>
2014-10-19 00:17:22 -0700

    pothos-library: cleanup proxy-server monitor implementation

--- pothos-library/apps/PothosUtilProxyServer.cpp
+++ pothos-library/apps/PothosUtilProxyServer.cpp
@@ -2,25 +2,78 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #include "PothosUtil.hpp"
+#include <Pothos/Init.hpp>
 #include <Pothos/Remote.hpp>
-#include <Pothos/Plugin/Loader.hpp>
 #include <Poco/Net/ServerSocket.h>
 #include <Poco/Net/SocketStream.h>
 #include <Poco/Net/TCPServer.h>
 #include <Poco/Process.h>
 #include <Poco/URI.h>
-#include <chrono>
-#include <thread>
+#include <mutex>
+#include <cassert>
 #include <iostream>
 
+/***********************************************************************
+ * TCP connection factory
+ *  - create connection handler threads
+ *  - monitor connection start and stop
+ *  - kill process in require active mode
+ **********************************************************************/
+class MyTCPServerConnectionFactory : public Poco::Net::TCPServerConnectionFactory
+{
+public:
+    MyTCPServerConnectionFactory(const bool requireActive):
+        _numConnections(0),
+        _requireActive(requireActive)
+    {
+        return;
+    }
+
+    Poco::Net::TCPServerConnection *createConnection(const Poco::Net::StreamSocket &socket);
+
+    void connectionStart(void)
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        _numConnections++;
+    }
+
+    void connectionStop(void)
+    {
+        std::unique_lock<std::mutex> lock(_mutex);
+        assert(_numConnections != 0);
+        _numConnections--;
+        if (_numConnections == 0 and _requireActive)
+        {
+            std::cerr << "Proxy server: No active connections - killing server" << std::endl;
+            Poco::Process::kill(Poco::Process::id());
+        }
+    }
+
+private:
+    std::mutex _mutex;
+    size_t _numConnections;
+    const bool _requireActive;
+};
+
+/***********************************************************************
+ * TCP connection thread
+ *  - create the remote handler for the connection
+ *  - service the remote handler until disconnect
+ **********************************************************************/
 class MyTCPServerConnection : public Poco::Net::TCPServerConnection
 {
 public:
-    MyTCPServerConnection(const Poco::Net::StreamSocket &socket):
+    MyTCPServerConnection(MyTCPServerConnectionFactory &factory, const Poco::Net::StreamSocket &socket):
         Poco::Net::TCPServerConnection(socket),
+        _factory(factory),
         _handler(Pothos::RemoteHandler(socket.peerAddress().host().toString()))
     {
-        return;
+        _factory.connectionStart();
+    }
+
+    ~MyTCPServerConnection(void)
+    {
+        _factory.connectionStop();
     }
 
     void run(void)
@@ -30,50 +83,18 @@ public:
     }
 
 private:
+    MyTCPServerConnectionFactory &_factory;
     Pothos::RemoteHandler _handler;
 };
 
-class MyTCPServerConnectionFactory : public Poco::Net::TCPServerConnectionFactory
+Poco::Net::TCPServerConnection *MyTCPServerConnectionFactory::createConnection(const Poco::Net::StreamSocket &socket)
 {
-public:
-    MyTCPServerConnectionFactory(void)
-    {
-        return;
-    }
+    return new MyTCPServerConnection(*this, socket);
+}
 
-    Poco::Net::TCPServerConnection *createConnection(const Poco::Net::StreamSocket &socket)
-    {
-        return new MyTCPServerConnection(socket);
-    }
-};
-
-struct MyTCPConnectionMonitor: public Poco::Runnable
-{
-    MyTCPConnectionMonitor(Poco::Net::TCPServer &server):
-        running(true),
-        server(server)
-    {
-        return;
-    }
-
-    virtual void run(void)
-    {
-        while (running)
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-            if (server.currentConnections() == 0)
-            {
-                std::cerr << "Proxy server: No active connections - killing server" << std::endl;
-                Poco::Process::kill(Poco::Process::id());
-                return;
-            }
-        }
-    }
-
-    bool running;
-    Poco::Net::TCPServer &server;
-};
-
+/***********************************************************************
+ * Spawn TCP proxy server given server URI
+ **********************************************************************/
 void PothosUtilBase::proxyServer(const std::string &, const std::string &uriStr)
 {
     //set stdio to be unbuffered to prevent IO backup when this is a subprocess
@@ -96,33 +117,16 @@ void PothosUtilBase::proxyServer(const std::string &, const std::string &uriStr)
     //create server socket
     Poco::Net::SocketAddress sa(host, port);
     Poco::Net::ServerSocket serverSocket(sa);
-    Poco::Net::TCPServerConnectionFactory::Ptr factory(new MyTCPServerConnectionFactory());
+    Poco::Net::TCPServerConnectionFactory::Ptr factory;
+    factory = new MyTCPServerConnectionFactory(this->config().hasOption("requireActive"));
     Poco::Net::TCPServer tcpServer(factory, serverSocket);
 
     //start the server
-    std::cout << "Host: " << serverSocket.address().host().toString() << std::endl;
-    std::cout << "Port: " << serverSocket.address().port() << std::endl;
     serverSocket.listen();
     tcpServer.start();
+    std::cout << "Host: " << serverSocket.address().host().toString() << std::endl;
+    std::cout << "Port: " << serverSocket.address().port() << std::endl;
 
-    //monitor active connections in monitor mode
-    if (this->config().hasOption("requireActive"))
-    {
-        //create a TCP connection monitor thread
-        MyTCPConnectionMonitor monitor(tcpServer);
-        Poco::Thread monitorThread;
-        monitorThread.start(monitor);
-
-        //wait here until the term signal is received
-        this->waitForTerminationRequest();
-
-        //end the monitor thread
-        monitor.running = false;
-        monitorThread.join();
-    }
-    else
-    {
-        //wait here until the term signal is received
-        this->waitForTerminationRequest();
-    }
+    //wait here until the term signal is received
+    this->waitForTerminationRequest();
 }
