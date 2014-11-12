a592089e4c5c302118279543e6d96760a8a32fdb
Josh Blum <josh@joshknows.com>
2014-11-12 15:25:42 -0800

    pothos-library: backup non-numa allocation functions

--- pothos-library/lib/Framework/WindowsDelayLoadedSymbols.cpp
+++ pothos-library/lib/Framework/WindowsDelayLoadedSymbols.cpp
@@ -3,6 +3,7 @@
 
 #include <Pothos/Config.hpp>
 #include <windows.h>
+#include <iostream>
 #include <cassert>
 
 /***********************************************************************
@@ -12,17 +13,19 @@ static FARPROC GetKernelProcAddress(LPCSTR lpProcName)
 {
     static HMODULE hKernel = LoadLibrary("kernel32.dll");
     assert(hKernel);
-    return GetProcAddress(hKernel, lpProcName);
+    FARPROC r = GetProcAddress(hKernel, lpProcName);
+    if (not r) std::cerr << "This kernel32 does not support " << lpProcName << std::endl;
+    return r;
 }
 
 /***********************************************************************
- * delay load various symbols
+ * numa information
  **********************************************************************/
 BOOL DL_GetNumaAvailableMemoryNodeEx(USHORT Node, PULONGLONG AvailableBytes)
 {
     typedef BOOL (WINAPI * GetNumaAvailableMemoryNodeEx_t)(USHORT, PULONGLONG);
     static auto fcn = (GetNumaAvailableMemoryNodeEx_t)GetKernelProcAddress("GetNumaAvailableMemoryNodeEx");
-    if (fcn == nullptr) return false;
+    if (not fcn) return false;
     return fcn(Node, AvailableBytes);
 }
 
@@ -30,23 +33,26 @@ BOOL DL_GetNumaNodeProcessorMaskEx(USHORT Node, PGROUP_AFFINITY ProcessorMask)
 {
     typedef BOOL (WINAPI * GetNumaNodeProcessorMaskEx_t)(USHORT, PGROUP_AFFINITY);
     static auto fcn = (GetNumaNodeProcessorMaskEx_t)GetKernelProcAddress("GetNumaNodeProcessorMaskEx");
-    if (fcn == nullptr) return false;
+    if (not fcn) return false;
     return fcn(Node, ProcessorMask);
 }
 
+/***********************************************************************
+ * numa allocation -- with non-numa backup support
+ **********************************************************************/
 LPVOID DL_VirtualAllocExNuma(HANDLE hProcess, LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect, DWORD nndPreferred)
 {
     typedef LPVOID (WINAPI * VirtualAllocExNuma_t)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD, DWORD);
     static auto fcn = (VirtualAllocExNuma_t)GetKernelProcAddress("VirtualAllocExNuma");
-    if (fcn == nullptr) return nullptr;
+    if (not fcn) return VirtualAllocEx(hProcess, lpAddress, dwSize, flAllocationType, flProtect);
     return fcn(hProcess, lpAddress, dwSize, flAllocationType, flProtect, nndPreferred);
 }
 
 HANDLE DL_CreateFileMappingNuma(HANDLE hFile, LPSECURITY_ATTRIBUTES lpFileMappingAttributes, DWORD flProtect, DWORD dwMaximumSizeHigh, DWORD dwMaximumSizeLow, LPCTSTR lpName, DWORD nndPreferred)
 {
     typedef HANDLE (WINAPI * CreateFileMappingNuma_t)(HANDLE, LPSECURITY_ATTRIBUTES, DWORD, DWORD, DWORD, LPCTSTR, DWORD);
-    static auto fcn = (CreateFileMappingNuma_t)GetKernelProcAddress("CreateFileMappingNuma");
-    if (fcn == nullptr) return nullptr;
+    static auto fcn = (CreateFileMappingNuma_t)GetKernelProcAddress("CreateFileMappingNumaA");
+    if (not fcn) return CreateFileMapping(hFile, lpFileMappingAttributes, flProtect, dwMaximumSizeHigh, dwMaximumSizeLow, lpName);
     return fcn(hFile, lpFileMappingAttributes, flProtect, dwMaximumSizeHigh, dwMaximumSizeLow, lpName, nndPreferred);
 }
 
@@ -54,6 +60,6 @@ LPVOID DL_MapViewOfFileExNuma(HANDLE hFileMappingObject, DWORD dwDesiredAccess, 
 {
     typedef LPVOID (WINAPI * MapViewOfFileExNuma_t)(HANDLE, DWORD, DWORD, DWORD, SIZE_T, LPVOID, DWORD);
     static auto fcn = (MapViewOfFileExNuma_t)GetKernelProcAddress("MapViewOfFileExNuma");
-    if (fcn == nullptr) return nullptr;
+    if (not fcn) return MapViewOfFileEx(hFileMappingObject, dwDesiredAccess, dwFileOffsetHigh, dwFileOffsetLow, dwNumberOfBytesToMap, lpBaseAddress);
     return fcn(hFileMappingObject, dwDesiredAccess, dwFileOffsetHigh, dwFileOffsetLow, dwNumberOfBytesToMap, lpBaseAddress, nndPreferred);
 }
