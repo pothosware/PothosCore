3224a181681ad686c7e7a2b96d7dfe32a1cf8ab4
Josh Blum <josh@joshknows.com>
2014-11-23 01:32:57 -0800

    pothos-widgets: added constellation plotter

---
+++ pothos-widgets/Constellation/Constellation.cpp
@@ -0,0 +1,137 @@
+// Copyright (c) 2014-2014 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include "ConstellationDisplay.hpp"
+#include <Pothos/Framework.hpp>
+#include <iostream>
+
+/***********************************************************************
+ * |PothosDoc Constellation
+ *
+ * The periodogram plot displays a live constellation plot of a complex signal.
+ *
+ * |category /Widgets
+ * |keywords plot constellation scatter
+ *
+ * |param title The title of the plot
+ * |default "Constellation"
+ * |widget StringEntry()
+ *
+ * |param displayRate[Display Rate] How often the plotter updates.
+ * |default 10.0
+ * |units updates/sec
+ *
+ * |param numPoints[Num Points] The number of points per plot capture.
+ * |default 1024
+ *
+ * |param autoScale[Auto-Scale] Enable automatic scaling for the vertical axis.
+ * |default false
+ * |option [Auto scale] true
+ * |option [Use limits] false
+ * |preview disable
+ *
+ * |param xRange[X-Axis Range] The minimum and maximum values for the X-Axis.
+ * When auto scale is off, this parameter controls the horizontal axis.
+ * |default [-1.5, 1.5]
+ * |preview disable
+ *
+ * |param yRange[Y-Axis Range] The minimum and maximum values for the Y-Axis.
+ * When auto scale is off, this parameter controls the vertical axis.
+ * |default [-1.5, 1.5]
+ * |preview disable
+ *
+ * |param enableXAxis[Enable X-Axis] Show or hide the horizontal axis markers.
+ * |option [Show] true
+ * |option [Hide] false
+ * |default true
+ * |preview disable
+ *
+ * |param enableYAxis[Enable Y-Axis] Show or hide the vertical axis markers.
+ * |option [Show] true
+ * |option [Hide] false
+ * |default true
+ * |preview disable
+ *
+ * |mode graphWidget
+ * |factory /widgets/constellation(remoteEnv)
+ * |setter setTitle(title)
+ * |setter setDisplayRate(displayRate)
+ * |setter setNumPoints(numPoints)
+ * |setter setAutoScale(autoScale)
+ * |setter setXRange(xRange)
+ * |setter setYRange(yRange)
+ * |setter enableXAxis(enableXAxis)
+ * |setter enableYAxis(enableYAxis)
+ **********************************************************************/
+class Constellation : public Pothos::Topology
+{
+public:
+    static Topology *make(const Pothos::ProxyEnvironment::Sptr &remoteEnv)
+    {
+        return new Constellation(remoteEnv);
+    }
+
+    Constellation(const Pothos::ProxyEnvironment::Sptr &remoteEnv)
+    {
+        _display.reset(new ConstellationDisplay());
+
+        auto registry = remoteEnv->findProxy("Pothos/BlockRegistry");
+        _snooper = registry.callProxy("/blocks/stream_snooper");
+
+        //register calls in this topology
+        this->registerCall(this, POTHOS_FCN_TUPLE(Constellation, setDisplayRate));
+        this->registerCall(this, POTHOS_FCN_TUPLE(Constellation, setNumPoints));
+
+        //connect to internal display block
+        this->connect(this, "setTitle", _display, "setTitle");
+        this->connect(this, "setAutoScale", _display, "setAutoScale");
+        this->connect(this, "setXRange", _display, "setXRange");
+        this->connect(this, "setYRange", _display, "setYRange");
+        this->connect(this, "enableXAxis", _display, "enableXAxis");
+        this->connect(this, "enableYAxis", _display, "enableYAxis");
+
+        //connect to the internal snooper block
+        this->connect(this, "setDisplayRate", _snooper, "setTriggerRate");
+        this->connect(this, "setNumPoints", _snooper, "setChunkSize");
+
+        //connect stream ports
+        this->connect(this, 0, _snooper, 0);
+        this->connect(_snooper, 0, _display, 0);
+    }
+
+    Pothos::Object opaqueCallMethod(const std::string &name, const Pothos::Object *inputArgs, const size_t numArgs) const
+    {
+        //calls that go to the topology
+        try
+        {
+            return Pothos::Topology::opaqueCallMethod(name, inputArgs, numArgs);
+        }
+        catch (const Pothos::BlockCallNotFound &){}
+
+        //forward everything else to display
+        return _display->opaqueCallMethod(name, inputArgs, numArgs);
+    }
+
+    void setDisplayRate(const double rate)
+    {
+        _snooper.callVoid("setTriggerRate", rate);
+    }
+
+    void setNumPoints(const size_t num)
+    {
+        _display->setName(this->getName()+"Display");
+        _snooper.callVoid("setName", this->getName()+"Snooper");
+
+        _snooper.callVoid("setChunkSize", num);
+    }
+
+private:
+    Pothos::Proxy _snooper;
+    std::shared_ptr<ConstellationDisplay> _display;
+};
+
+/***********************************************************************
+ * registration
+ **********************************************************************/
+static Pothos::BlockRegistry registerConstellation(
+    "/widgets/constellation", &Constellation::make);
---
+++ pothos-widgets/Constellation/ConstellationDisplay.cpp
@@ -0,0 +1,117 @@
+// Copyright (c) 2014-2014 Josh Blum
+// SPDX-License-Identifier: BSL-1.0
+
+#include "ConstellationDisplay.hpp"
+#include "MyPlotStyler.hpp"
+#include "MyPlotPicker.hpp"
+#include "MyPlotUtils.hpp"
+#include <QResizeEvent>
+#include <qwt_plot.h>
+#include <qwt_plot_grid.h>
+#include <QHBoxLayout>
+
+ConstellationDisplay::ConstellationDisplay(void):
+    _mainPlot(new MyQwtPlot(this)),
+    _plotGrid(new QwtPlotGrid()),
+    _zoomer(new MyPlotPicker(_mainPlot->canvas())),
+    _autoScale(false)
+{
+    //setup block
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, widget));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, setTitle));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, setAutoScale));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, title));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, autoScale));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, setXRange));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, setYRange));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, enableXAxis));
+    this->registerCall(this, POTHOS_FCN_TUPLE(ConstellationDisplay, enableYAxis));
+    this->setupInput(0);
+
+    //layout
+    auto layout = new QHBoxLayout(this);
+    layout->setSpacing(0);
+    layout->setContentsMargins(QMargins());
+    layout->addWidget(_mainPlot);
+
+    //setup plotter
+    {
+        _mainPlot->setCanvasBackground(MyPlotCanvasBg());
+        connect(_zoomer, SIGNAL(zoomed(const QRectF &)), this, SLOT(handleZoomed(const QRectF &)));
+        _mainPlot->setAxisFont(QwtPlot::xBottom, MyPlotAxisFontSize());
+        _mainPlot->setAxisFont(QwtPlot::yLeft, MyPlotAxisFontSize());
+    }
+
+    //setup grid
+    {
+        _plotGrid->attach(_mainPlot);
+        _plotGrid->setPen(MyPlotGridPen());
+    }
+}
+
+ConstellationDisplay::~ConstellationDisplay(void)
+{
+    return;
+}
+
+void ConstellationDisplay::setTitle(const QString &title)
+{
+    QMetaObject::invokeMethod(_mainPlot, "setTitle", Qt::QueuedConnection, Q_ARG(QwtText, MyPlotTitle(title)));
+}
+
+void ConstellationDisplay::setAutoScale(const bool autoScale)
+{
+    _autoScale = autoScale;
+    QMetaObject::invokeMethod(this, "handleUpdateAxis", Qt::QueuedConnection);
+}
+
+void ConstellationDisplay::setXRange(const std::vector<double> &range)
+{
+    if (range.size() != 2) throw Pothos::RangeException("ConstellationDisplay::setXRange()", "range vector must be size 2");
+    _xRange = range;
+    QMetaObject::invokeMethod(this, "handleUpdateAxis", Qt::QueuedConnection);
+}
+
+void ConstellationDisplay::setYRange(const std::vector<double> &range)
+{
+    if (range.size() != 2) throw Pothos::RangeException("ConstellationDisplay::setYRange()", "range vector must be size 2");
+    _yRange = range;
+    QMetaObject::invokeMethod(this, "handleUpdateAxis", Qt::QueuedConnection);
+}
+
+void ConstellationDisplay::handleUpdateAxis(void)
+{
+    if (_xRange.size() == 2) _mainPlot->setAxisScale(QwtPlot::xBottom, _xRange[0], _xRange[1]);
+    if (_yRange.size() == 2) _mainPlot->setAxisScale(QwtPlot::yLeft, _yRange[0], _yRange[1]);
+
+    _mainPlot->setAxisTitle(QwtPlot::xBottom, QString("In-Phase"));
+    _mainPlot->setAxisTitle(QwtPlot::yLeft, QString("Quadrature"));
+
+    _mainPlot->updateAxes(); //update after axis changes
+    _zoomer->setZoomBase(); //record current axis settings
+    this->handleZoomed(_zoomer->zoomBase()); //reload
+}
+
+void ConstellationDisplay::handleZoomed(const QRectF &rect)
+{
+    //when zoomed all the way out, return to autoscale
+    if (rect == _zoomer->zoomBase() and _autoScale)
+    {
+        _mainPlot->setAxisAutoScale(QwtPlot::yLeft);
+    }
+}
+
+QString ConstellationDisplay::title(void) const
+{
+    return _mainPlot->title().text();
+}
+
+void ConstellationDisplay::enableXAxis(const bool enb)
+{
+    _mainPlot->enableAxis(QwtPlot::xBottom, enb);
+}
+
+void ConstellationDisplay::enableYAxis(const bool enb)
+{
+    _mainPlot->enableAxis(QwtPlot::yLeft, enb);
+}
