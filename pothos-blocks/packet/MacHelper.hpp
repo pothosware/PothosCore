7402efd10b0786b5a7627a7f6fe8615ef81dc462
Josh Blum <josh@joshknows.com>
2015-01-25 17:22:08 -0800

    pothos-blocks: work on MAC unpack packet

--- pothos-blocks/packet/MacHelper.hpp
+++ pothos-blocks/packet/MacHelper.hpp
@@ -10,5 +10,33 @@ struct MacHeader
     uint16_t crc; //the crc8 checksum
     uint16_t id; //the sender's ID number
     uint16_t seq; //packet sequence number
-    uint16_t bytes; //the number of payload bytes
+    uint16_t bytes; //the total number of bytes
 };
+
+//https://chromium.googlesource.com/chromiumos/platform/vboot_reference/+/master/firmware/lib/crc8.c
+/* Copyright (c) 2013 The Chromium OS Authors. All rights reserved.
+* Use of this source code is governed by a BSD-style license that can be
+* found in the LICENSE file.
+*/
+
+#include <cstdint>
+
+/**
+* Return CRC-8 of the data, using x^8 + x^2 + x + 1 polynomial. A table-based
+* algorithm would be faster, but for only a few bytes it isn't worth the code
+* size. */
+inline uint8_t Crc8(const void *vptr, int len)
+{
+    const uint8_t *data = (const uint8_t *)vptr;
+    unsigned crc = 0;
+    int i, j;
+    for (j = len; j; j--, data++) {
+    crc ^= (*data << 8);
+    for(i = 8; i; i--) {
+    if (crc & 0x8000)
+    crc ^= (0x1070 << 3);
+    crc <<= 1;
+    }
+    }
+    return (uint8_t)(crc >> 8);
+}
--- pothos-blocks/packet/SimpleMac.cpp
+++ pothos-blocks/packet/SimpleMac.cpp
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: BSL-1.0
 
 #include <Pothos/Framework.hpp>
+#include "MacHelper.hpp"
 
 /***********************************************************************
  * |PothosDoc Simple MAC
@@ -19,7 +20,10 @@
 class SimpleMac : public Pothos::Block
 {
 public:
-    SimpleMac(void)
+    SimpleMac(void):
+        _seqNo(0),
+        _id(0),
+        _errorCount(0)
     {
         this->setupInput("phyIn");
         this->setupInput("macIn");
@@ -32,10 +36,65 @@ public:
         return new SimpleMac();
     }
 
+    void activate(void)
+    {
+        _phyIn = this->input("phyIn");
+        _macIn = this->input("macIn");
+        _phyOut = this->output("phyOut");
+        _macOut = this->output("macOut");
+    }
+
+    Pothos::BufferChunk unpack(const Pothos::Packet &pkt)
+    {
+        const auto hdr = pkt.payload.as<const MacHeader *>();
+
+        //short packet or bad length
+        if (pkt.payload.length < hdr->bytes) return Pothos::BufferChunk();
+
+        //check the id
+        if (hdr->id != _id) return Pothos::BufferChunk();
+
+        //check crc
+        auto hdrCrc = hdr->crc;
+        const_cast<MacHeader *>(hdr)->crc = 0;
+        auto newCrc = Crc8(pkt.payload.as<const void *>(), hdr->bytes);
+        if (newCrc != hdrCrc) return Pothos::BufferChunk();
+
+        //return the payload
+        auto payload = pkt.payload;
+        payload.length = hdr->bytes - sizeof(MacHeader);
+        payload.address += sizeof(MacHeader);
+        return payload;
+    }
+
     void work(void)
     {
-        
+        //check phy input packets for crc and send to the mac out
+        if (_phyIn->hasMessage())
+        {
+            auto msg = _phyIn->popMessage();
+            auto pktIn = msg.extract<Pothos::Packet>();
+            Pothos::Packet pktOut;
+            pktOut.payload = this->unpack(pktIn);
+            if (pktOut.payload) _macOut->postMessage(pktOut);
+        }
+
+        //mac input packets are protocol framed and sent to the phy out
+        if (_macIn->hasMessage())
+        {
+            auto msg = _macIn->popMessage();
+            auto pktIn = msg.extract<Pothos::Packet>();
+        }
     }
+
+private:
+    size_t _seqNo;
+    size_t _id;
+    unsigned long long _errorCount;
+    Pothos::OutputPort *_phyOut;
+    Pothos::OutputPort *_macOut;
+    Pothos::InputPort *_phyIn;
+    Pothos::InputPort *_macIn;
 };
 
 static Pothos::BlockRegistry registerSimpleMac(
