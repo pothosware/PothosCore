6c9869aa03714b7f79b56ca99b832f97662e5cce
Josh Blum <josh@joshknows.com>
2015-03-13 06:25:11 -0700

    pothos-widgets: work on logic analyzer

--- pothos-widgets/LogicAnalyzer/LogicAnalyzer.cpp
+++ pothos-widgets/LogicAnalyzer/LogicAnalyzer.cpp
@@ -20,17 +20,106 @@
  * |preview disable
  *
  * |param displayRate[Display Rate] How often the plotter updates.
- * |default 10.0
+ * |default 1.0
  * |units updates/sec
  *
+ * |param sampleRate[Sample Rate] The rate of the input elements.
+ * |default 1e6
+ * |units samples/sec
+ *
  * |param numPoints[Num Points] The number of points per plot capture.
- * |default 1024
+ * |default 256
+ *
+ * |param label0[Ch0 Label] The display label for channel 0.
+ * |default "Ch0"
+ * |widget StringEntry()
+ * |preview disable
+ * |tab Channels
+ *
+ * |param base0[Ch0 Base] The numeric base for channel 0.
+ * |default 10
+ * |option [Binary] 2
+ * |option [Octal] 8
+ * |option [Decimal] 10
+ * |option [Hexadecimal] 16
+ * |preview disable
+ * |tab Channels
+ *
+ * |param label1[Ch1 Label] The display label for channel 1.
+ * |default "Ch1"
+ * |widget StringEntry()
+ * |preview disable
+ * |tab Channels
+ *
+ * |param base1[Ch1 Base] The numeric base for channel 1.
+ * |default 10
+ * |option [Binary] 2
+ * |option [Octal] 8
+ * |option [Decimal] 10
+ * |option [Hexadecimal] 16
+ * |preview disable
+ * |tab Channels
+ *
+ * |param label2[Ch2 Label] The display label for channel 2.
+ * |default "Ch2"
+ * |widget StringEntry()
+ * |preview disable
+ * |tab Channels
+ *
+ * |param base2[Ch2 Base] The numeric base for channel 2.
+ * |default 10
+ * |option [Binary] 2
+ * |option [Octal] 8
+ * |option [Decimal] 10
+ * |option [Hexadecimal] 16
+ * |preview disable
+ * |tab Channels
+ *
+ * |param label3[Ch2 Label] The display label for channel 3.
+ * |default "Ch3"
+ * |widget StringEntry()
+ * |preview disable
+ * |tab Channels
+ *
+ * |param base3[Ch3 Base] The numeric base for channel 3.
+ * |default 10
+ * |option [Binary] 2
+ * |option [Octal] 8
+ * |option [Decimal] 10
+ * |option [Hexadecimal] 16
+ * |preview disable
+ * |tab Channels
+ *
+ * |param xAxisMode[X Axis Mode] Choose between index or time display.
+ * |option [Index] "INDEX"
+ * |option [Time] "TIME"
+ * |default "INDEX"
+ * |preview disable
+ * |tab Axis
+ *
+ * |param rateLabelId[Rate Label ID] Labels with this ID can be used to set the sample rate.
+ * To ignore sample rate labels, set this parameter to an empty string.
+ * |default "rxRate"
+ * |widget StringEntry()
+ * |preview disable
+ * |tab Labels
  *
  * |mode graphWidget
  * |factory /widgets/logic_analyzer(remoteEnv)
  * |initializer setNumInputs(numInputs)
  * |setter setDisplayRate(displayRate)
+ * |setter setSampleRate(sampleRate)
  * |setter setNumPoints(numPoints)
+ * |setter setChannelLabel(0, label0)
+ * |setter setChannelBase(0, base0)
+ * |setter setChannelLabel(1, label1)
+ * |setter setChannelBase(1, base1)
+ * |setter setChannelLabel(2, label2)
+ * |setter setChannelBase(2, base2)
+ * |setter setChannelLabel(3, label3)
+ * |setter setChannelBase(3, base3)
+ * |setter setXAxisMode(xAxisMode)
+ * |setter setRateLabelId(rateLabelId)
  **********************************************************************/
 class LogicAnalyzer : public Pothos::Topology
 {
@@ -51,6 +140,11 @@ public:
         this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzer, setNumInputs));
         this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzer, setDisplayRate));
         this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzer, setNumPoints));
+
+        //connect to internal display block
+        this->connect(this, "setChannelLabel", _display, "setChannelLabel");
+        this->connect(this, "setChannelBase", _display, "setChannelBase");
+        this->connect(this, "setXAxisMode", _display, "setXAxisMode");
 
         //connect to the internal snooper block
         this->connect(this, "setDisplayRate", _snooper, "setTriggerRate");
--- pothos-widgets/LogicAnalyzer/LogicAnalyzerDisplay.cpp
+++ pothos-widgets/LogicAnalyzer/LogicAnalyzerDisplay.cpp
@@ -3,26 +3,54 @@
 
 #include "LogicAnalyzerDisplay.hpp"
 #include <QTableWidget>
-#include <QTabWidget>
 #include <QHBoxLayout>
 #include <complex>
 
+/***********************************************************************
+ * Number format overloads
+ **********************************************************************/
+static QString toStr(const std::complex<qreal> num, const int)
+{
+    if (num.real() == 0.0 and num.imag() == 0.0) return "0";
+    if (num.real() == 0.0) return QString::number(num.imag())+"j";
+    if (num.imag() == 0.0) return QString::number(num.real());
+    if (num.imag() < 0.0) return QString("%1%2j").arg(num.real()).arg(num.imag());
+    return QString("%1+%2j").arg(num.real()).arg(num.imag());
+}
+
+static QString toStr(const qreal num, const int)
+{
+    return QString::number(num);
+}
+
+static QString toStr(const qlonglong num, const int base)
+{
+    return QString::number(num, base);
+}
+
+/***********************************************************************
+ * Logic analyzer GUI implementation
+ **********************************************************************/
 LogicAnalyzerDisplay::LogicAnalyzerDisplay(void):
-    _viewTabs(new QTabWidget(this)),
-    _waveView(new QTableWidget(this)),
-    _listView(new QTableWidget(this))
+    _tableView(new QTableWidget(this)),
+    _sampleRate(1.0),
+    _xAxisMode("INDEX"),
+    _rateLabelId("rxRate")
 {
     auto layout = new QHBoxLayout(this);
-    layout->addWidget(_viewTabs);
+    layout->addWidget(_tableView);
 
     //setup block
     this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, widget));
     this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, setNumInputs));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, numInputs));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, setSampleRate));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, sampleRate));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, setChannelLabel));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, setChannelBase));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, setXAxisMode));
+    this->registerCall(this, POTHOS_FCN_TUPLE(LogicAnalyzerDisplay, setRateLabelId));
     this->setupInput(0);
-
-    //build gui
-    _viewTabs->addTab(_waveView, tr("Wave View"));
-    _viewTabs->addTab(_listView, tr("List View"));
 
     //register types passed to gui thread from work
     qRegisterMetaType<Pothos::BufferChunk>("Pothos::BufferChunk");
@@ -39,59 +67,101 @@ void LogicAnalyzerDisplay::setNumInputs(const size_t numInputs)
     {
         this->setupInput(i, this->input(0)->dtype());
     }
-    this->initTables();
+
+    _chLabel.resize(numInputs);
+    _chBase.resize(numInputs, 10);
+    _chBuffers.resize(numInputs);
+    _tableView->setRowCount(numInputs);
 }
 
-void LogicAnalyzerDisplay::initTables(void)
+void LogicAnalyzerDisplay::setSampleRate(const double sampleRate)
 {
-    _waveView->clear();
-    _listView->clear();
-
-    const size_t numChans(this->inputs().size());
-
-    _waveView->setRowCount(numChans);
-    _listView->setColumnCount(numChans);
-
-    for (size_t i = 0; i < numChans; i++)
-    {
-        _waveView->setVerticalHeaderItem(i, new QTableWidgetItem(tr("Ch%1").arg(i)));
-        _listView->setHorizontalHeaderItem(i, new QTableWidgetItem(tr("Ch%1").arg(i)));
-    }
-}
-
-template <typename T>
-QString toStr(const std::complex<T> num)
-{
-    return QString("%1 + %2j").arg(num.real()).arg(num.imag());
-}
-
-template <typename T>
-QString toStr(const T num)
-{
-    return QString::number(num);
+    _sampleRate = sampleRate;
+    QMetaObject::invokeMethod(this, "handleReplot", Qt::QueuedConnection);
 }
 
 template <typename T>
 void LogicAnalyzerDisplay::populateChannel(const int channel, const Pothos::BufferChunk &buffer)
 {
     const auto numericBuff = buffer.convert(typeid(T));
+    _chBuffers[channel] = numericBuff;
     for (size_t i = 0; i < buffer.elements(); i++)
     {
         const auto num = numericBuff.as<const T *>()[i];
-        const auto s = toStr(num);
-        _waveView->setItem(channel, i, new QTableWidgetItem(s));
-        _listView->setItem(i, channel, new QTableWidgetItem(s));
+        const auto s = toStr(num, _chBase.at(channel));
+        auto item = new QTableWidgetItem(s);
+        auto flags = item->flags();
+        flags &= ~Qt::ItemIsEditable;
+        item->setFlags(flags);
+        _tableView->setItem(channel, i, item);
     }
 }
 
 void LogicAnalyzerDisplay::updateData(const int channel, const Pothos::BufferChunk &buffer)
 {
-    _waveView->setColumnCount(buffer.elements());
-    _listView->setRowCount(buffer.elements());
+    //column count changed? new labels
+    if (_tableView->columnCount() != int(buffer.elements()))
+    {
+        double factor = 1.0;
+        QString units("s");
+        double timeSpan = buffer.elements()/_sampleRate;
+        if (timeSpan <= 100e-9)
+        {
+            factor = 1e9;
+            units = "ns";
+        }
+        else if (timeSpan <= 100e-6)
+        {
+            factor = 1e6;
+            units = "us";
+        }
+        else if (timeSpan <= 100e-3)
+        {
+            factor = 1e3;
+            units = "ms";
+        }
 
-    if (buffer.dtype.isComplex()) this->populateChannel<std::complex<double>>(channel, buffer);
-    else if (buffer.dtype.isFloat()) this->populateChannel<double>(channel, buffer);
-    else if (buffer.dtype.isInteger()) this->populateChannel<long long>(channel, buffer);
+        _tableView->setColumnCount(buffer.elements());
+        if (_xAxisMode == "INDEX")
+        {
+            for (size_t i = 0; i < buffer.elements(); i++)
+            {
+                _tableView->setHorizontalHeaderItem(i, new QTableWidgetItem(QString::number(i)));
+            }
+        }
+        if (_xAxisMode == "TIME")
+        {
+            for (size_t i = 0; i < buffer.elements(); i++)
+            {
+                double t = i*_sampleRate/factor;
+                _tableView->setHorizontalHeaderItem(i, new QTableWidgetItem(QString::number(t)+units));
+            }
+        }
+    }
+
+    if (buffer.dtype.isComplex()) this->populateChannel<std::complex<qreal>>(channel, buffer);
+    else if (buffer.dtype.isFloat()) this->populateChannel<qreal>(channel, buffer);
+    else if (buffer.dtype.isInteger()) this->populateChannel<qlonglong>(channel, buffer);
+}
+
+void LogicAnalyzerDisplay::handleReplot(void)
+{
+    _tableView->clear();
+    _tableView->setColumnCount(0);
+
+    const int numChans(this->inputs().size());
+
+    _tableView->setRowCount(numChans);
+
+    for (int ch = 0; ch < numChans; ch++)
+    {
+        auto label = _chLabel.at(ch);
+        if (label.isEmpty()) label = tr("Ch%1").arg(ch);
+        _tableView->setVerticalHeaderItem(ch, new QTableWidgetItem(label));
+        this->updateData(ch, _chBuffers.at(ch));
+    }
+
+    _tableView->resizeColumnsToContents();
 }
 
 void LogicAnalyzerDisplay::work(void)
@@ -100,6 +170,16 @@ void LogicAnalyzerDisplay::work(void)
     {
         if (not inPort->hasMessage()) continue;
         const auto msg = inPort->popMessage();
+
+        //label-based messages have in-line commands
+        if (msg.type() == typeid(Pothos::Label))
+        {
+            const auto &label = msg.convert<Pothos::Label>();
+            if (label.id == _rateLabelId and label.data.canConvert(typeid(double)))
+            {
+                this->setSampleRate(label.data.convert<double>());
+            }
+        }
 
         //packet-based messages have payloads to display
         if (msg.type() == typeid(Pothos::Packet))
