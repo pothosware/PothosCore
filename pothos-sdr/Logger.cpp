7a0b028004ee0705cecf9b13be85e03234cb6953
Josh Blum <josh@joshknows.com>
2015-03-14 15:29:17 -0700

    pothos-sdr: logger prints for streaming status indicator

--- pothos-sdr/Logger.cpp
+++ pothos-sdr/Logger.cpp
@@ -1,9 +1,10 @@
-// Copyright (c) 2014-2014 Josh Blum
+// Copyright (c) 2014-2015 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 #include <Pothos/Plugin.hpp>
 #include <SoapySDR/Logger.hpp>
 #include <Poco/Logger.h>
+#include <iostream>
 
 /***********************************************************************
  * Register Poco logging handler for SoapySDR
@@ -14,6 +15,11 @@ static void SoapyPocoLogHandler(const SoapySDR::LogLevel logLevel, const char *m
     static_assert(Poco::Message::Priority(SOAPY_SDR_FATAL) == Poco::Message::PRIO_FATAL, "SoapySDR log levels match Poco");
     static_assert(Poco::Message::Priority(SOAPY_SDR_INFO) == Poco::Message::PRIO_INFORMATION, "SoapySDR log levels match Poco");
     static_assert(Poco::Message::Priority(SOAPY_SDR_TRACE) == Poco::Message::PRIO_TRACE, "SoapySDR log levels match Poco");
+    if (logLevel == SOAPY_SDR_SSI)
+    {
+        std::cerr << message << std::flush;
+        return;
+    }
     logger.log(Poco::Message("SoapySDR", message, Poco::Message::Priority(logLevel)));
 }
 
