28117aefd0eee87fe791ce0631d2af44c3cf6826
Josh Blum <josh@joshknows.com>
2015-03-26 15:12:49 -0700

    pothos-util: removed extra typo semicolon BlockEval

--- pothos-util/BlockEval.hpp
+++ pothos-util/BlockEval.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2014 Josh Blum
+// Copyright (c) 2014-2015 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 #pragma once
@@ -18,7 +18,7 @@ public:
         _evalEnv(env)
     {
         return;
-    };
+    }
 
     Pothos::Object evalProperty(const std::string &key, const std::string &expr)
     {
