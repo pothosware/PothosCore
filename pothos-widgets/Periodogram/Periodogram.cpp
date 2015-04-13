d6af956dd9ee33570eeda330a0d969c359bc9eb6
Josh Blum <josh@joshknows.com>
2015-04-13 02:00:48 -0700

    pothos-widgets: set shorter names once on init for displays
    This is OK now that the DOT markup (from JSON dump work) has hierarchical names.

--- pothos-widgets/Periodogram/Periodogram.cpp
+++ pothos-widgets/Periodogram/Periodogram.cpp
@@ -149,9 +149,11 @@ public:
     Periodogram(const Pothos::ProxyEnvironment::Sptr &remoteEnv)
     {
         _display.reset(new PeriodogramDisplay());
+        _display->setName("Display");
 
         auto registry = remoteEnv->findProxy("Pothos/BlockRegistry");
         _snooper = registry.callProxy("/blocks/stream_snooper");
+        _snooper.callVoid("setName", "Snooper");
 
         //register calls in this topology
         this->registerCall(this, POTHOS_FCN_TUPLE(Periodogram, setNumInputs));
@@ -194,9 +196,6 @@ public:
 
     void setNumInputs(const size_t numInputs)
     {
-        _display->setName(this->getName()+"Display");
-        _snooper.callVoid("setName", this->getName()+"Snooper");
-
         _display->setNumInputs(numInputs);
         _snooper.callVoid("setNumPorts", numInputs);
         for (size_t i = 0; i < numInputs; i++)
