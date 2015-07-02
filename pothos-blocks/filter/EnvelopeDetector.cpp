ab57e8a66e122ee69f1bfccb48f6908aee0a4d1f
Josh Blum <josh@joshknows.com>
2015-07-02 14:54:19 -0700

    pothos-blocks: separate envelope detector and threshold

--- pothos-blocks/filter/EnvelopeDetector.cpp
+++ pothos-blocks/filter/EnvelopeDetector.cpp
@@ -10,12 +10,6 @@
 
 /***********************************************************************
  * |PothosDoc Envelope Detector
- * 
- * The envelope detector block calculates the envelope of an input signal
- * and checks the envelope against a configurable threshold to detect
- * and forward useful samples that fall within the activation zone.
- *
- * <h2>Envelope Calculation</h2>
  *
  * The envelope calculator consumes samples from input port 0
  * and computes the envelope using a single pole filter.
@@ -24,80 +18,34 @@
  *
  * https://en.wikipedia.org/wiki/Envelope_detector
  *
- * <h2>Threshold Operation</h2>
- *
- * The threshold operation compares the calculated envelope
- * against configurable activation and deactivation thresholds
- * to determine regions within the signal that are "useful".
- *
- * The operation forwards samples from input port 0 to output port "th"
- * and adds optional annotation labels to the output stream
- * to indicate threshold activation and deactivation points.
- * The sample forwarding can operate in several modes:
- *
- * <ul>
- * <li><b>PASS</b> - All input samples are passed to the output.
- *  There is a 1:1 ratio between input and output samples.</li>
- * <li><b>ZERO</b> - Samples outside of the threshold are zeroed.
- *  There is a 1:1 ratio between input and output samples.</li>
- * <li><b>DROP</b> - Samples outside of the threshold are dropped.
- *  There is a reduction in the number of output samples.</li>
- * </ul>
- *
  * |category /Filter
- * |keywords filter envelope threshold attack decay sustain release
+ * |keywords filter envelope attack decay sustain release lookahead
  *
  * |param dtype[Input Type] The data type of the input stream.
  * |widget DTypeChooser(float=1,cfloat=1,int=1,cint=1)
  * |default "complex_float64"
  * |preview disable
  *
- * |param attack[Attack Duration] The run-up time constant in samples.
- * Single pole filter roll-off constant: alphaAttack = exp(-1/attack)
+ * |param attack The run-up time constant in samples.
+ * Single pole filter roll-off constant: gainAttack = exp(-1/attack)
  * |default 10
  * |units samples
  *
- * |param release[Release Duration] The decay time constant in samples.
- * Single pole filter roll-off constant: alphaRelease = exp(-1/release)
+ * |param release The decay time constant in samples.
+ * Single pole filter roll-off constant: gainRelease = exp(-1/release)
  * |default 10
  * |units samples
  *
- * |param thresholdMode[Threshold Mode] The operation mode for the threshold output data port.
- * |default "PASS"
- * |option [Pass] "PASS"
- * |option [Zero] "ZERO"
- * |option [Drop] "DROP"
- * |tab Threshold
- *
- * |param activationThresh[Activation Threshold] The threshold that the envelope must exceed to activate.
- * |default 0.5
- * |tab Threshold
- *
- * |param deactivationThresh[Deactivation Threshold] The threshold that the envelope must fall-below to deactivate.
- * |default 0.5
- * |tab Threshold
- *
- * |param activationId[Activation ID] The label ID to mark the element that crosses the activation threshold (when inactive).
- * An empty string (default) means that activate labels are not produced.
- * |default ""
- * |widget StringEntry()
- * |preview valid
- * |tab Threshold
- *
- * |param deactivationId[Deactivation ID] The label ID to mark the element that crosses the deactivation threshold (when active).
- * An empty string (default) means that deactivate labels are not produced.
- * |default ""
- * |widget StringEntry()
- * |preview valid
- * |tab Threshold
+ * |param lookahead A configurable input delay to compensate for envelope lag.
+ * Without lookahead, the envelope calculation lags behind the input due to filtering.
+ * The lookahead compensation adjusts the envelope to match up with the input events.
+ * |default 10
+ * |units samples
  *
  * |factory /blocks/envelope_detector(dtype)
  * |setter setAttack(attack)
  * |setter setRelease(release)
- * |setter setActivationThreshold(activationThresh)
- * |setter setDeactivationThreshold(deactivationThresh)
- * |setter setActivationId(activationId)
- * |setter setDeactivationId(deactivationId)
+ * |setter setLookahead(lookahead)
  **********************************************************************/
 template <typename InType, typename OutType>
 class EnvelopeDetector : public Pothos::Block
@@ -105,63 +53,56 @@ class EnvelopeDetector : public Pothos::Block
 public:
     EnvelopeDetector(void):
         _envelope(0),
-        _attackAlpha(0),
-        _releaseAlpha(0),
-        _oneMinusAttackAlpha(0),
-        _oneMinusReleaseAlpha(0),
-        _activationThreshold(0),
-        _deactivationThreshold(0),
-        _activeState(false),
-        _theshOutPort(nullptr)
+        _attack(0),
+        _release(0),
+        _lookahead(0),
+        _attackGain(0),
+        _releaseGain(0),
+        _oneMinusAttackGain(0),
+        _oneMinusReleaseGain(0)
     {
         this->setupInput(0, typeid(InType));
         this->setupOutput(0, typeid(OutType));
-        this->setupOutput("th", typeid(InType), this->uid()); //unique domain because of buffer forwarding
         this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setAttack));
+        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, getAttack));
         this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setRelease));
-        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setActivationThreshold));
-        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setDeactivationThreshold));
-        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setActivationId));
-        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setDeactivationId));
-        _theshOutPort = this->output("th");
+        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, getRelease));
+        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, setLookahead));
+        this->registerCall(this, POTHOS_FCN_TUPLE(EnvelopeDetector, getLookahead));
     }
 
     void setAttack(const OutType attack)
     {
-        _attackAlpha = std::exp(-1/attack);
-        _oneMinusAttackAlpha = 1-_attackAlpha;
+        _attack = attack;
+        _attackGain = std::exp(-1/attack);
+        _oneMinusAttackGain = 1-_attackGain;
+    }
+
+    OutType getAttack(void) const
+    {
+        return _attack;
     }
 
     void setRelease(const OutType release)
     {
-        _releaseAlpha = std::exp(-1/release);
-        _oneMinusReleaseAlpha = 1-_releaseAlpha;
+        _release = release;
+        _releaseGain = std::exp(-1/release);
+        _oneMinusReleaseGain = 1-_releaseGain;
     }
 
-    void setActivationThreshold(const OutType thresh)
+    OutType getRelease(void) const
     {
-        _activationThreshold = thresh;
+        return _release;
     }
 
-    void setDeactivationThreshold(const OutType thresh)
+    void setLookahead(const OutType lookahead)
     {
-        _deactivationThreshold = thresh;
+        _lookahead = lookahead;
     }
 
-    void setActivationId(const std::string id)
+    OutType getLookahead(void) const
     {
-        _activationId = id;
-    }
-
-    void setDeactivationId(const std::string id)
-    {
-        _deactivationId = id;
-    }
-
-    void activate(void)
-    {
-        //reset state before running
-        _activeState = false;
+        return _lookahead;
     }
 
     void work(void)
@@ -169,75 +110,57 @@ public:
         auto inPort = this->input(0);
         auto outPort = this->output(0);
 
-        const auto in = inPort->buffer().template as<const InType *>();
-        const auto out = outPort->buffer().template as<OutType *>();
-        const size_t N = std::min(inPort->elements(), outPort->elements());
+        //ensure that the input has lookahead room
+        if (inPort->elements() <= _lookahead)
+        {
+            inPort->setReserve(_lookahead+1);
+            return;
+        }
+
+        //calculate the work size given the available resources
+        const size_t N = std::min(inPort->elements()-_lookahead, outPort->elements());
         if (N == 0) return;
 
+        //cast the input and output buffers
+        const auto in = inPort->buffer().template as<const InType *>();
+        const auto out = outPort->buffer().template as<OutType *>();
+
+        //perform envelope calculation operation
         for (size_t i = 0; i < N; i++)
         {
-            //envelope calculation
-            const OutType xn = OutType(std::abs(in[i]));
+            const OutType xn = OutType(std::abs(in[i+_lookahead]));
             if (xn > _envelope)
             {
-                _envelope = _attackAlpha*_envelope + _oneMinusAttackAlpha*xn;
+                _envelope = _attackGain*_envelope + _oneMinusAttackGain*xn;
             }
             else
             {
-                _envelope = _releaseAlpha*_envelope + _oneMinusReleaseAlpha*xn;
+                _envelope = _releaseGain*_envelope + _oneMinusReleaseGain*xn;
             }
             out[i] = _envelope;
-
-            //threshold operation
-            if (not _activeState and _envelope > _activationThreshold)
-            {
-                _activeState = true;
-                if (not _activationId.empty())
-                {
-                    _theshOutPort->postLabel(Pothos::Label(_activationId, Pothos::Object(), i));
-                }
-            }
-            else if (_activeState and _envelope < _deactivationThreshold)
-            {
-                _activeState = false;
-                if (not _deactivationId.empty())
-                {
-                    _theshOutPort->postLabel(Pothos::Label(_deactivationId, Pothos::Object(), i));
-                }
-            }
         }
 
+        //produce/consume the work size
         inPort->consume(N);
         outPort->produce(N);
-
-        auto buff = inPort->buffer();
-        buff.length = N*buff.dtype.size();
-        _theshOutPort->postBuffer(buff);
     }
 
-/*
-    void propagateLabels(const Pothos::InputPort *port)
+    //! use a circular buffer to implement lookahead window efficiently
+    Pothos::BufferManager::Sptr getInputBufferManager(const std::string &, const std::string &)
     {
-        auto outputPort = this->output(0);
-        for (const auto &label : port->labels())
-        {
-        }
+        return Pothos::BufferManager::make("circular");
     }
-    */
 
 private:
 
     OutType _envelope;
-    OutType _attackAlpha;
-    OutType _releaseAlpha;
-    OutType _oneMinusAttackAlpha;
-    OutType _oneMinusReleaseAlpha;
-    OutType _activationThreshold;
-    OutType _deactivationThreshold;
-    std::string _activationId;
-    std::string _deactivationId;
-    bool _activeState;
-    Pothos::OutputPort *_theshOutPort;
+    OutType _attack;
+    OutType _release;
+    size_t _lookahead;
+    OutType _attackGain;
+    OutType _releaseGain;
+    OutType _oneMinusAttackGain;
+    OutType _oneMinusReleaseGain;
 };
 
 /***********************************************************************
