a6075893e9ed5fa793d2f029cdba5a3e3a5776e6
Josh Blum <josh@joshknows.com>
2015-07-03 01:48:19 -0700

    pothos-blocks: stream to packet unit test

--- pothos-blocks/packet/TestPacketBlocks.cpp
+++ pothos-blocks/packet/TestPacketBlocks.cpp
@@ -86,3 +86,50 @@ POTHOS_TEST_BLOCK("/blocks/tests", test_packet_to_stream)
     POTHOS_TEST_EQUAL(labels[1].index, p0.payload.elements()-1);
     POTHOS_TEST_EQUAL(labels[1].width, 1);
 }
+
+POTHOS_TEST_BLOCK("/blocks/tests", test_stream_to_packet)
+{
+    auto env = Pothos::ProxyEnvironment::make("managed");
+    auto registry = env->findProxy("Pothos/BlockRegistry");
+
+    //create the blocks
+    auto feeder = registry.callProxy("/blocks/feeder_source", "int");
+    auto collector = registry.callProxy("/blocks/collector_sink", "int");
+    auto s2p = registry.callProxy("/blocks/stream_to_packet");
+    s2p.callVoid("setFrameStartId", "SOF0");
+    s2p.callVoid("setFrameEndId", "EOF0");
+
+    //create test data
+    Pothos::BufferChunk b0("int", 100);
+    for (size_t i = 0; i < b0.elements(); i++)
+        b0.as<int *>()[i] = std::rand();
+    feeder.callVoid("feedBuffer", b0);
+    const size_t sofIndex = 14;
+    const size_t eofIndex = 77;
+    feeder.callVoid("feedLabel", Pothos::Label("NOPE", Pothos::Object(), sofIndex-10));
+    feeder.callVoid("feedLabel", Pothos::Label("SOF0", Pothos::Object(), sofIndex));
+    feeder.callVoid("feedLabel", Pothos::Label("NOPE", Pothos::Object(), (eofIndex+sofIndex)/2));
+    feeder.callVoid("feedLabel", Pothos::Label("EOF0", Pothos::Object(), eofIndex));
+    feeder.callVoid("feedLabel", Pothos::Label("NOPE", Pothos::Object(), eofIndex+10));
+
+    //create the topology
+    Pothos::Topology topology;
+    topology.connect(feeder, 0, s2p, 0);
+    topology.connect(s2p, 0, collector, 0);
+    topology.commit();
+    POTHOS_TEST_TRUE(topology.waitInactive());
+
+    //check the result
+    const auto packets = collector.call<std::vector<Pothos::Packet>>("getPackets");
+    POTHOS_TEST_EQUAL(packets.size(), 1);
+    const auto packet = packets.at(0);
+    POTHOS_TEST_EQUAL(packet.labels.size(), 3);
+    POTHOS_TEST_EQUAL(packet.labels[0].id, "SOF0");
+    POTHOS_TEST_EQUAL(packet.labels[0].index, 0);
+    POTHOS_TEST_EQUAL(packet.labels[1].id, "NOPE");
+    POTHOS_TEST_EQUAL(packet.labels[1].index, (eofIndex-sofIndex)/2);
+    POTHOS_TEST_EQUAL(packet.labels[2].id, "EOF0");
+    POTHOS_TEST_EQUAL(packet.labels[2].index, eofIndex-sofIndex);
+    POTHOS_TEST_EQUAL(packet.payload.elements(), eofIndex-sofIndex+1);
+    POTHOS_TEST_EQUALA(b0.as<const int *>()+sofIndex, packet.payload.as<const int *>(), packet.payload.elements());
+}
