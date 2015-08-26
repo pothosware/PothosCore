d26417c69a5e75fdd79ca2e3d7ca69315d93be67
Josh Blum <josh@joshknows.com>
2015-08-26 16:55:31 -0700

    0.2.1 docs update

---
+++ doxygen/0.2.1/search/classes_e.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['sharedbuffer',['SharedBuffer',['../classPothos_1_1SharedBuffer.html',1,'Pothos']]],
+  ['signalexception',['SignalException',['../classPothos_1_1SignalException.html',1,'Pothos']]],
+  ['spinlock',['SpinLock',['../classPothos_1_1Util_1_1SpinLock.html',1,'Pothos::Util']]],
+  ['syntaxexception',['SyntaxException',['../classPothos_1_1SyntaxException.html',1,'Pothos']]],
+  ['systemexception',['SystemException',['../classPothos_1_1SystemException.html',1,'Pothos']]]
+];
---
+++ doxygen/0.2.1/search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['const_5fiterator',['const_iterator',['../classPothos_1_1LabelIteratorRange.html#a9fbba2ef2c3f6422a9127bc9f8736ce5',1,'Pothos::LabelIteratorRange']]]
+];
