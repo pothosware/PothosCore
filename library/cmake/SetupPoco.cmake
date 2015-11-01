ccd423a1b590d38c88f8ae71afa6f11db353c428
Karl Semich <0xloem@gmail.com>
2015-10-31 22:44:20 -0400

    Only check version for FindPoco.cmake

--- library/cmake/SetupPoco.cmake
+++ library/cmake/SetupPoco.cmake
@@ -21,6 +21,20 @@ message(STATUS "PocoConfig not found - trying with FindPoco")
 list(INSERT CMAKE_MODULE_PATH 0 ${CMAKE_CURRENT_LIST_DIR})
 find_package(Poco)
 if (POCO_FOUND AND POCO_Util_FOUND AND POCO_Net_FOUND AND POCO_XML_FOUND AND POCO_JSON_FOUND)
+    ########################################################################
+    # Check version >= 1.5.4
+    ########################################################################
+    FILE (READ "${POCO_INCLUDE_DIRS}/Poco/Version.h" Poco_VERSION)
+    set(_ws "[ \r\t\n^$]+")
+    STRING (REGEX REPLACE "^.*#define${_ws}POCO_VERSION${_ws}(0x[0-9a-fA-F]*)${_ws}.*$" "\\1" POCO_VERSION "${Poco_VERSION}")
+    set(_ws)
+    if ("${POCO_VERSION}" LESS 0x01050400)
+        return()
+    endif()
+
+    ########################################################################
+    # Set variables
+    ########################################################################
     set(Poco_FOUND 1)
     set(Poco_INCLUDE_DIRS ${POCO_INCLUDE_DIRS})
     set(Poco_LIBRARIES
@@ -41,12 +55,4 @@ if (POCO_FOUND AND POCO_Util_FOUND AND POCO_Net_FOUND AND POCO_XML_FOUND AND POC
         find_package(Threads)
         list(APPEND Poco_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
     endif()
-endif()
-
-if (Poco_FOUND)
-    FILE (READ "${Poco_INCLUDE_DIRS}/Poco/Version.h" Poco_VERSION)
-    set(_ws "[ \r\t\n^$]+")
-    STRING (REGEX REPLACE "^.*#define${_ws}POCO_VERSION${_ws}(0x[0-9a-fA-F]*)${_ws}.*$" "\\1" Poco_VERSION "${Poco_VERSION}")
-    set(_ws)
-    message(STATUS "Poco_VERSION: ${Poco_VERSION}")
 endif()
