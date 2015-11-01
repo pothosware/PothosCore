e26cf4dd01ea3414c0dc18c8650873e35e5958b7
Karl Semich <0xloem@gmail.com>
2015-10-31 22:39:48 -0400

    Revert 'Check version for both poco checks'

--- library/cmake/SetupPoco.cmake
+++ library/cmake/SetupPoco.cmake
@@ -11,35 +11,35 @@ find_package(Poco CONFIG COMPONENTS Foundation Util JSON XML Net)
 if (Poco_FOUND)
     message(STATUS "Poco_DIR: ${Poco_DIR}")
     message(STATUS "Poco_LIBRARIES: ${Poco_LIBRARIES}")
+    return()
+endif()
 
 ########################################################################
 # Find Poco with FindPoco.cmake (local copy)
 ########################################################################
-else()
-    message(STATUS "PocoConfig not found - trying with FindPoco")
-    list(INSERT CMAKE_MODULE_PATH 0 ${CMAKE_CURRENT_LIST_DIR})
-    find_package(Poco)
-    if (POCO_FOUND AND POCO_Util_FOUND AND POCO_Net_FOUND AND POCO_XML_FOUND AND POCO_JSON_FOUND)
-        set(Poco_FOUND 1)
-        set(Poco_INCLUDE_DIRS ${POCO_INCLUDE_DIRS})
-        set(Poco_LIBRARIES
-            ${POCO_LIBRARIES}
-            ${POCO_Util_LIBRARIES}
-            ${POCO_Net_LIBRARIES}
-            ${POCO_XML_LIBRARIES}
-            ${POCO_JSON_LIBRARIES}
-        )
-        message(STATUS "Poco_INCLUDE_DIRS: ${Poco_INCLUDE_DIRS}")
-        message(STATUS "Poco_LIBRARIES: ${Poco_LIBRARIES}")
-    
-        ########################################################################
-        # Link with the thread library
-        ########################################################################
-        if (UNIX)
-            set(CMAKE_THREAD_PREFER_PTHREAD ON)
-            find_package(Threads)
-            list(APPEND Poco_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
-        endif()
+message(STATUS "PocoConfig not found - trying with FindPoco")
+list(INSERT CMAKE_MODULE_PATH 0 ${CMAKE_CURRENT_LIST_DIR})
+find_package(Poco)
+if (POCO_FOUND AND POCO_Util_FOUND AND POCO_Net_FOUND AND POCO_XML_FOUND AND POCO_JSON_FOUND)
+    set(Poco_FOUND 1)
+    set(Poco_INCLUDE_DIRS ${POCO_INCLUDE_DIRS})
+    set(Poco_LIBRARIES
+        ${POCO_LIBRARIES}
+        ${POCO_Util_LIBRARIES}
+        ${POCO_Net_LIBRARIES}
+        ${POCO_XML_LIBRARIES}
+        ${POCO_JSON_LIBRARIES}
+    )
+    message(STATUS "Poco_INCLUDE_DIRS: ${Poco_INCLUDE_DIRS}")
+    message(STATUS "Poco_LIBRARIES: ${Poco_LIBRARIES}")
+
+    ########################################################################
+    # Link with the thread library
+    ########################################################################
+    if (UNIX)
+        set(CMAKE_THREAD_PREFER_PTHREAD ON)
+        find_package(Threads)
+        list(APPEND Poco_LIBRARIES ${CMAKE_THREAD_LIBS_INIT})
     endif()
 endif()
 
