e95d8f3049e45b9363322a3c56a9d2f5b5c3bfbe
Tony Kirke <tony@pyspuc.com>
2015-11-13 15:59:06 -0800

    For bandstop transforms, negate and then add 1 to midtap of bandpass

--- comms/spuce/filters/transform_fir.cpp
+++ comms/spuce/filters/transform_fir.cpp
@@ -7,30 +7,31 @@ namespace spuce {
 //! \author Tony Kirke
 //! \ingroup functions fir
 std::vector<float_type> transform_fir(const std::string& band_type, const std::vector<float_type>& taps, float_type f) {
-	std::vector<float_type> out(taps.size());
+  std::vector<float_type> out(taps.size());
   if (band_type == "LOW_PASS") {
-		// just copy
-		for (size_t i=0;i<taps.size();i++) {
-			out[i] = taps[i];
-		}
+    // just copy
+    for (size_t i=0;i<taps.size();i++) {
+      out[i] = taps[i];
+    }
   } else if (band_type == "HIGH_PASS") {
-		float_type h = 1;
-		// Alternate multiplying by +1,-1
-		for (size_t i=0;i<taps.size();i++) {
-			out[i] = h*taps[i];
-			h *= -1;
-		}
+    float_type h = 1;
+    // Alternate multiplying by +1,-1
+    for (size_t i=0;i<taps.size();i++) {
+      out[i] = h*taps[i];
+      h *= -1;
+    }
   } else if (band_type == "BAND_PASS") {
-		// Modulate taps up to band center frequency
-		for (size_t i=0;i<taps.size();i++) {
-			// Scale to restore center tap value & by 0.5 due to cosine multiplication
-			out[i] = cos(2.0*M_PI*f*i) * taps[i] / (0.5*cos(M_PI*f*(taps.size()-1)));
-		}
+    // Modulate taps up to band center frequency
+    for (size_t i=0;i<taps.size();i++) {
+      // Scale to restore center tap value & by 0.5 due to cosine multiplication
+      out[i] = cos(2.0*M_PI*f*i) * taps[i] / (0.5*cos(M_PI*f*(taps.size()-1)));
+    }
   } else if (band_type == "BAND_STOP") {
-		// First transform to band_pass, then subtract 1 from center tap
-		out = transform_fir("BAND_PASS", taps, f);
-		size_t mid_tap = (taps.size()-1)/2;
-    out[mid_tap] -= 1;
+    // First transform to band_pass, then negate and add 1 at center tap
+    out = transform_fir("BAND_PASS", taps, f);
+    for (size_t i=0;i<taps.size();i++) out[i] *= -1;
+    size_t mid_tap = (taps.size()-1)/2;
+    out[mid_tap] += 1;
   } else {
     std::cout << "Unsupported band type :" << band_type << "\n";
   }
@@ -45,12 +46,13 @@ std::vector<std::complex<float_type>> transform_complex_fir(const std::string& b
       out[i] = taps[i]*std::polar(1.0, phase);
     }
   } else if (band_type == "COMPLEX_BAND_STOP") {
-		out = transform_complex_fir("COMPLEX_BAND_PASS", taps, f);
-		size_t mid_tap = (taps.size()-1)/2;
-		out[mid_tap] -= 1;
+    out = transform_complex_fir("COMPLEX_BAND_PASS", taps, f);
+    for (size_t i=0;i<taps.size();i++) out[i] *= -1;
+    size_t mid_tap = (taps.size()-1)/2;
+    out[mid_tap] += 1;
   } else {
     std::cout << "Unsupported band type :" << band_type << "\n";
   }
-	return out;
+  return out;
 }
 }  // namespace spuce
