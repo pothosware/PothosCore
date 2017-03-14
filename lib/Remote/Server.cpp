7f75a287a577be8779e78ac097c5fde732c2aaa0
Josh Blum <josh@joshknows.com>
2017-03-14 13:20:21 -0700

    Fixed RemoteServer process arguments for some shells
    The separate args did not work on freebsd.
    We could not even pass them on via command line.
    It may be a shell difference, but using an equals
    converts the proxy address into a single argument.

--- lib/Remote/Server.cpp
+++ lib/Remote/Server.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2013-2016 Josh Blum
+// Copyright (c) 2013-2017 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 #include <Pothos/Remote.hpp>
@@ -70,8 +70,7 @@ Pothos::RemoteServer::RemoteServer(const std::string &uriStr, const bool closePi
     //create args
     Poco::Process::Args args;
     args.push_back("--require-active");
-    args.push_back("--proxy-server");
-    args.push_back(uriStr);
+    args.push_back("--proxy-server="+uriStr);
 
     //launch
     Poco::Pipe outPipe, errPipe;
