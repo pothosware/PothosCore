b0cfda3d0a0e294920d01304f02e3e53c527a28e
Josh Blum <josh@joshknows.com>
2017-09-21 22:18:17 -0500

    Fix getApiVersion() string format/extraction
    * The masks were incorrect (was half the width)
    * Use hex format so it has the same values in the string

--- lib/System/Version.in.cpp
+++ lib/System/Version.in.cpp
@@ -1,15 +1,17 @@
-// Copyright (c) 2013-2016 Josh Blum
+// Copyright (c) 2013-2017 Josh Blum
 // SPDX-License-Identifier: BSL-1.0
 
 #include <Pothos/System/Version.hpp>
-#include <Poco/Format.h>
+#include <sstream>
 
 std::string Pothos::System::getApiVersion(void)
 {
-    return Poco::format("%d.%d.%d",
-        int((POTHOS_API_VERSION >> 24) & 0xf),
-        int((POTHOS_API_VERSION >> 16) & 0xf),
-        int((POTHOS_API_VERSION >> 0) & 0xff));
+    std::stringstream ss;
+    ss << std::hex << int((POTHOS_API_VERSION >> 24) & 0xff) << "."
+       << std::hex << int((POTHOS_API_VERSION >> 16) & 0xff) << "."
+       << std::hex << int((POTHOS_API_VERSION >> 0) & 0xffff)
+       << std::dec;
+    return ss.str();
 }
 
 std::string Pothos::System::getAbiVersion(void)
