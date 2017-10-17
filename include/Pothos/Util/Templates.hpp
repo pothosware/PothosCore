c9b46925416da11e08cb3e61aa7d3b2675fbc10f
Josh Blum <josh@joshknows.com>
2017-10-17 02:34:16 -0500

    doxygen comments for templates hpp work

--- include/Pothos/Util/Templates.hpp
+++ include/Pothos/Util/Templates.hpp
@@ -16,43 +16,66 @@
 namespace Pothos {
 namespace Util {
 
-/***********************************************************************
+/*!*********************************************************************
+ * \defgroup unwrapref Implementation for unwrap_refwrapper and special_decay_t
  * special_decay_t = decay + unwrapping a reference wrapper
- * http://en.cppreference.com/w/cpp/utility/tuple/make_tuple
+ * \see http://en.cppreference.com/w/cpp/utility/tuple/make_tuple
+ * \{
  **********************************************************************/
+
+/*!
+ * Template base implementation to extract the type of non-reference wrappers
+ * \tparam T the input type to the template
+ */
 template <typename T>
 struct unwrap_refwrapper
 {
-    using type = T;
+    using type = T; //!< the input type of non-reference wrappers
 };
 
+/*!
+ * Template specialization to extract the type held within a reference wrapper
+ * \tparam T the input type to the template
+ */
 template <typename T>
 struct unwrap_refwrapper<std::reference_wrapper<T>>
 {
-    using type = T&;
+    using type = T&; //!< the type held within a reference wrapper
 };
 
+/*!
+ * Strip const and reference from a type, and remove optional reference wrapper.
+ * \tparam T the input type to the template
+ */
 template <typename T>
 using special_decay_t = typename unwrap_refwrapper<typename std::decay<T>::type>::type;
+//! \}
 
-/***********************************************************************
- * Integer sequence implementation for C++11
+/*!*********************************************************************
+ * \defgroup intseq Integer sequence implementation for C++11
  * This can be removed when C++14 is required.
- * http://en.cppreference.com/w/cpp/utility/integer_sequence
+ * \see http://en.cppreference.com/w/cpp/utility/integer_sequence
+ * \{
  **********************************************************************/
-//! \cond
 
+/*!
+ * A parameter pack of incrementing numbers 0 to N-1
+ * \tparam T the integer type
+ * \tparam Ints the parameter pack of integers
+ */
 template<typename T, T... Ints>
 struct integer_sequence
 {
-    typedef T value_type;
+    typedef T value_type; //!< The type of each integer
 
+    //! The size of the integer pack
     static constexpr std::size_t size(void) noexcept
     {
         return sizeof...(Ints);
     }
 };
 
+//! \cond
 template<typename T, int N, T... Ints>
 struct GenSeq : GenSeq<T, N-1, T(N-1), Ints...> {};
 
@@ -61,20 +84,37 @@ struct GenSeq<T, 0, Ints...>
 {
     typedef integer_sequence<T, Ints...> Type;
 };
+//! \endcond
 
+/*!
+ * Specialize the integer sequence for size_t
+ * \tparam Ints the parameter pack of integers
+ */
 template<std::size_t... Ints>
 using index_sequence = integer_sequence<std::size_t, Ints...>;
 
+/*!
+ * Make an integer sequence from the length
+ * \tparam T the integer type
+ * \tparam N the length
+ */
 template<typename T, T N>
 using make_integer_sequence = typename GenSeq<T, int(N)>::Type;
 
+/*!
+ * Make an index sequence from the length
+ * \tparam N the length
+ */
 template<std::size_t N>
 using make_index_sequence = make_integer_sequence<std::size_t, N>;
 
+/*!
+ * Make an index sequence from a parameter pack
+ * \tparam T the parameter pack
+ */
 template<typename... T>
 using index_sequence_for = make_index_sequence<sizeof...(T)>;
-
-//! \endcond
+//! \}
 
 } //namespace Util
 } //namespace Pothos
