eb19d9762d361636d370a2231fc7d9f23b1ba1b2
Nicholas Corgan <n.corgan@gmail.com>
2020-10-04 16:43:53 -0500

    BufferConvert: hide SFINAE

--- lib/SIMD/BufferConvert.cpp
+++ lib/SIMD/BufferConvert.cpp
@@ -5,7 +5,6 @@
 
 #include <complex>
 #include <type_traits>
-#include <vector>
 
 #if !defined POTHOS_SIMD_NAMESPACE
 #error Must define POTHOS_SIMD_NAMESPACE to build this file
@@ -13,57 +12,60 @@
 
 namespace PothosSIMD { namespace POTHOS_SIMD_NAMESPACE {
 
-    template <typename T>
-    struct IsComplex : std::false_type {};
-
-    template <typename T>
-    struct IsComplex<std::complex<T>> : std::true_type {};
-
-    template <typename InType, typename OutType>
-    using EnableIfNeitherComplex = typename std::enable_if<!IsComplex<InType>::value && !IsComplex<OutType>::value>::type;
-
-    template <typename InType, typename OutType>
-    using EnableIfBothComplex = typename std::enable_if<IsComplex<InType>::value && IsComplex<OutType>::value>::type;
-
-    template <typename InType, typename OutType>
-    static EnableIfNeitherComplex<InType, OutType> _bufferConvert(const InType* in, OutType* out, size_t len)
+    namespace detail
     {
-        // Use input size, XSIMD will deal with the output batch sizes internally.
-        static constexpr size_t simdSize = xsimd::simd_traits<InType>::size;
-        const auto numSIMDFrames = len / simdSize;
+        template <typename T>
+        struct IsComplex : std::false_type {};
 
-        const InType* inPtr = in;
-        OutType* outPtr = out;
+        template <typename T>
+        struct IsComplex<std::complex<T>> : std::true_type {};
 
-        for (size_t frameIndex = 0; frameIndex < numSIMDFrames; ++frameIndex)
+        template <typename InType, typename OutType>
+        using EnableIfNeitherComplex = typename std::enable_if<!IsComplex<InType>::value && !IsComplex<OutType>::value>::type;
+
+        template <typename InType, typename OutType>
+        using EnableIfBothComplex = typename std::enable_if<IsComplex<InType>::value&& IsComplex<OutType>::value>::type;
+
+        template <typename InType, typename OutType>
+        static EnableIfNeitherComplex<InType, OutType> bufferConvert(const InType* in, OutType* out, size_t len)
         {
-            auto reg = xsimd::load_unaligned(inPtr);
-            reg.store_unaligned(outPtr);
+            // Use input size, XSIMD will deal with the output batch sizes internally.
+            static constexpr size_t simdSize = xsimd::simd_traits<InType>::size;
+            const auto numSIMDFrames = len / simdSize;
 
-            inPtr += simdSize;
-            outPtr += simdSize;
+            const InType* inPtr = in;
+            OutType* outPtr = out;
+
+            for (size_t frameIndex = 0; frameIndex < numSIMDFrames; ++frameIndex)
+            {
+                auto reg = xsimd::load_unaligned(inPtr);
+                reg.store_unaligned(outPtr);
+
+                inPtr += simdSize;
+                outPtr += simdSize;
+            }
+
+            // Perform remaining operations manually.
+            for (size_t elem = (simdSize * numSIMDFrames); elem < len; ++elem)
+            {
+                out[elem] = static_cast<OutType>(in[elem]);
+            }
         }
 
-        // Perform remaining operations manually.
-        for (size_t elem = (simdSize * numSIMDFrames); elem < len; ++elem)
+        template <typename InType, typename OutType>
+        static EnableIfBothComplex<InType, OutType> bufferConvert(const InType* in, OutType* out, size_t len)
         {
-            out[elem] = static_cast<OutType>(in[elem]);
+            using ScalarInType = typename InType::value_type;
+            using ScalarOutType = typename OutType::value_type;
+
+            bufferConvert<ScalarInType, ScalarOutType>((const ScalarInType*)in, (ScalarOutType*)out, (len * 2));
         }
-    }
-
-    template <typename InType, typename OutType>
-    static EnableIfBothComplex<InType, OutType> _bufferConvert(const InType* in, OutType* out, size_t len)
-    {
-        using ScalarInType = typename InType::value_type;
-        using ScalarOutType = typename OutType::value_type;
-
-        bufferConvert<ScalarInType, ScalarOutType>((const ScalarInType*)in, (ScalarOutType*)out, (len * 2));
     }
 
     template <typename InType, typename OutType>
     void bufferConvert(const void* in, void* out, size_t len)
     {
-        _bufferConvert<InType, OutType>((const InType*)in, (OutType*)out, len);
+        detail::bufferConvert<InType, OutType>((const InType*)in, (OutType*)out, len);
     }
 
 #define DECLARE_BUFFERCONVERT_FUNCS(InType) \
